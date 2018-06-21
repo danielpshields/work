33b3973d8b01d636ab0167941b4c3d4c0c69b59a
ahuntercatalano <alex.catalano2@gmail.com>
2018-06-21 08:41:30 -0400

    [LN] opened thursday

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
         <h4>June</h4>
         <h5>Week 5</h5>
         <ul>
+          <li><a href="ln/18/06/21.html">180621</a></li>
           <li><a href="ln/18/06/20.html">180620</a></li>
           <li><a href="ln/18/06/19.html">180619</a></li>
           <li><a href="ln/18/06/18.html">180618</a></li>
