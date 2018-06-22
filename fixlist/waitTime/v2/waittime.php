2dfda2eaf49b71c272900b431a1b29b0919ff879
ahuntercatalano <alex.catalano2@gmail.com>
2018-06-22 11:00:34 -0400

    [waittime] (11 a.m.)

--- fixlist/waitTime/v2/waittime.php
+++ fixlist/waitTime/v2/waittime.php
@@ -5,41 +5,18 @@
 	<link rel="stylesheet" href="waittime.css">
 </head>
 <body>
-	<h1>AAC Lobby Wait Time</h1>
-<table>
-	<tr>
-		<td>time: </td>
-		<td><?php echo $time; ?></td>
-	</tr>
-	<tr>
-		<td>right now: </td>
-		<td><?php echo $rightnow; ?></td>
-	</tr>
-	<tr>
-		<td>now: </td>
-		<td><?php echo $now; ?></td>
-	</tr>
-	<tr>
-		<td>my date: </td>
-		<td><?php echo $mydate; ?></td>
-	</tr>
-	<tr>
-		<td>is closed: </td>
-		<td><?php echo $isclosed; ?></td>
-	</tr>
-</table>
+	<h1>AAC Lobby Wait Time</h1><?php include("table.php"); ?>
 
 	<div class="lobbyWaitShell">
 		<h2>AAC Lobby Wait Time</h2>
 
 		<?php if ($lobbyIsOpen === "closed") { ?>
 
-
-			<!-- <div class="leftSide" id="closed">
+			<div class="leftSide" id="closed">
 				<p><img src="closed.jpg"></p>
-			</div> -->
+			</div>
 
-			<!-- <div class="rightSide">
+			<div class="rightSide">
 				<h3>Walk-in Hours</h3>
 				<p>mornings:</p>
 				<ul>
@@ -50,31 +27,28 @@
 					<li>1:30 <span class="tinyTime">p.m.</span> - 4:30 <span class="tinyTime">p.m.</span></li>
 				</ul>
 			</div>
-			<div class="clear"></div> -->
+			<div class="clear"></div>
 
 			<div class="currentTime">
 				<p>Advising sessions will resume at 8 a.m.</p>
 			</div>
 
-
-
-
-			<?php } else { ?>
-				<div id="leftSide">
+		<?php } else { ?>
+				<div class="leftSide">
 					<p id="currentStudent"><span class="bigNumber"><?php echo $students; ?></span> <span style="font-weight: 600;">students</span> waiting</p>
 					<p id="nextStudent">Next student has been waiting:</p>
 					<p><span class="mediumNumber darkRed"><?php echo $h . $m; ?></span> <span class="tiny">minutes</span></p>
 				</div>
 
-				<div id="rightSide">
+				<div class="rightSide">
 					<h3>Walk-in Hours</h3>
 					<p>mornings:</p>
 					<ul>
-						<li>8 a.m. - 11:30 a.m.</li>
+						<li>8:00 - 11:30</li>
 					</ul>
 					<p>afternoons:</p>
 					<ul>
-						<li>1:30 <span class="tinyTime">p.m.</span> - 4:30 <span class="tinyTime">p.m.</span></li>
+						<li>1:30 - 4:30</li>
 					</ul>
 				</div>
 				<div class="clear"></div>
@@ -87,6 +61,7 @@
 		</div><!-- end shell -->
 
 
+		<?php echo '<center><p><strong>AAC Lobby Wait Time</strong><br /><br />'.$display.'</p></center>'; ?>
 
 </body>
 </html>
