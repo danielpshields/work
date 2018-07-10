2c34a0d357edcfaa886240225665093b574363f9
ahuntercatalano <alex.catalano2@gmail.com>
2018-07-10 15:03:52 -0400

    fixlist [phpb] redirects set in place (3:03 p.m.)

--- fixlist/phpb/includes/header.php
+++ fixlist/phpb/includes/header.php
@@ -5,13 +5,11 @@
 		<meta name="keywords" content="Liberal Arts and Sciences, University, Humanities, Liberal Education, College, University of Florida, Gainesville, African and Asian Languages, Anthropology, Astronomy, Botany, Biology, Chemistry, Communication Sciences and Disorders, Criminology Law and Society, English, Geography, Geological Sciences, History, Mathematics, Philosophy, Physics, Political Science, Psychology, Religion, Romance Languages and Literatures, Sociology, Statistics, Zoology, African Studies, Bob Graham Center, European Studies, France Florida Research Institute, Greek Studies, Hindu Traditions, Jewish Studies, LUECI, Latin-American Studies, Medieval and Early Modern Studies, Paris Research Center, Women's Studies and Gender Research, Linguistics, African American Studies, Asian Studies, Linguistics, Oral History Program, Quantum Theory Project, Upward Bound Program, Academic Advising" />
 		<meta name="description" content="The UF College of Liberal Arts and Sciences constitutes the intellectual core of the university. Its principal mission is to lead the academic quest to understand our place in the universe, and to help shape our society and environment." />
 
-		<link rel="stylesheet" type="text/css" media="screen" href="css/basic.css" />
-
+		<link rel="stylesheet" href="css/basic.css">
 		<link rel="stylesheet" href="css/stylenew.css">
-
 		<link rel="stylesheet" href="css/index.css">
+		<link rel="stylesheet" href="css/<?php echo $page; ?>.css">
 
-		<link rel="stylesheet" href="css/<?php echo $page; ?>.css" />
 	</head>
 
 	<body>
