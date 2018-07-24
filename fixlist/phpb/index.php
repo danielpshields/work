025729c1c3e6396e031e69ff718667d409443848
ahuntercatalano <alex.catalano2@gmail.com>
2018-07-24 16:52:05 -0400

    [phpb] index page to mobile (180724, 4:52 p.m.)

--- fixlist/phpb/index.php
+++ fixlist/phpb/index.php
@@ -2,108 +2,107 @@
   $title="Pre-Health Post-baccalaureate Program (PHPB) | UF College of Liberal Arts and Sciences";
   $page = "index";
 ?>
+    <?php require_once("includes/header.php"); ?>
 
-<?php require_once("includes/header.php"); ?>
+      <div id="content">
 
-<div id="content">
+          <img src="images/header2.jpg" alt="College of Liberal Arts and Sciences Human Resources">
 
-  <img src="images/header2.jpg" alt="College of Liberal Arts and Sciences Human Resources">
+          <h1>Pre-Health Post-Baccalaureate Program <span class="small">(PHPB)</span></h1>
 
+          <p class="slug">"<i>Helping yourself is the first step in helping others.</i>"</p>
 
-  <h1>Pre-Health Post-Baccalaureate Program <span class="small">(PHPB)</span></h1>
+          <div class="highlight">
+            <h3>Prerequisites</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+          </div>
+          <div class="highlight">
+            <h3>Instruction</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+          <div class="highlight">
+            <h3>Duration</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+          <div class="highlight">
+            <h3>Costs</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+          </div>
 
-  <p class="slug">"<i>Helping yourself is the first step toward helping others</i>"</p>
+          <div class="apply">
+            <button onclick="location.href='apply.php';" type="button" name="button">Apply!</button>
+          </div>
 
-  <div id="highlights">
-    <div class="highlight">
-      <h2>Prerequisites</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
-    </div>
-    <div class="highlight">
-      <h2>Instruction</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    </div>
-    <div class="highlight">
-      <h2>Duration</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    </div>
-    <div class="highlight">
-      <h2>Costs</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    </div>
-  </div>
+          <h2>UF Pre-Health PostBac</h2>
 
+          <p>The University of Florida's Pre-health Post-Baccalaureate Program (PHPB) offers dedicated and motivated students an opportunity to complete academic pre-requisite courses for admission to medical, dental, or veterinary school. PHPB is a rigorous, two year structured program.</p>
+          <p>Courses are offered once per year. Students in PHPB complete the majority of courses in classes created just for PHPB students, experiencing the outstanding teaching faculty at the University of Florida. This is a comprehensive program offering non-science courses that assist you in developing academic skills, application skills, and professional competencies necessary for application to medical, dental, PA, or veterinary school.</p>
+          <p>Other health professions are considered. However, for professions other than medicine, pre-requisite courses not included in our standard curriculum are not offered.</p>
+          <p>Fall admission only</p>
 
-  <h2>UF Pre-Health PostBac</h2>
+          <section>
+            <p class="medium">2019</p>
+            <p id="applicationSlug"> Fall Application Cycle via <a href="https://postbaccas.liaisoncas.com/">PostBacCAS</a></p>
+            <p class="tiny light italic marginTop">Opens August 15, 2018</p>
+          </section>
 
-  <p>The University of Florida's Pre-health Post-Baccalaureate Program (PHPB) offers dedicated and motivated students an opportunity to complete academic pre-requisite courses for admission to medical, dental, or veterinary school. PHPB is a rigorous, two year structured program.</p>
-  <p>Courses are offered once per year.  Students in PHPB complete the majority of courses in classes created just for PHPB students, experiencing the outstanding teaching faculty at the University of Florida. This is a comprehensive program offering non-science courses that assist you in developing academic skills, application skills, and professional competencies necessary for application to medical, dental, PA, or veterinary school.</p>
-  <p>Other health professions are considered. However, for professions other than medicine, pre-requisite courses not included in our standard curriculum are not offered.</p>
-  <p><span id="applicationSlug">2019 Fall Application Cycle via <a href="https://postbaccas.liaisoncas.com/">PostBacCAS</a> opens August 15, 2018</span></p>
-  <h3>Tracks</h3>
-  <p>The UF PHPB program offers two tracks to help you complete pre-requisite coursework. Each track requires full-time enrollment. The track you choose depends on your academic background. All tracks require a bachelor's degree for admission. For more information on each track, see the Admission and Course information.  Fall admission only.</p>
-  <div class="pane">
-  <a href="admission-change.html">
-  <h3>Career Changer Program</h3>
-  <p>For students who have not taken more than two science pre-requisite courses earning Bs or above.</p>
-  <!--<img width="180px" height="150px" src="https://visualsonline.cancer.gov/retrieve.cfm?imageid=1847&amp;dpi=72&amp;fileformat=jpg">-->
-  </a>
-  </div>
-  <div class="pane">
-  <a href="admission-restart.html">
-  <h3>ReStart Program</h3>
-  <p>For students who took three or fewer science pre-requisite courses early in undergrad and earned less than a B without further attempts in science for two or more years.</p>
-  <!--<img width="180px" height="150px" src="https://visualsonline.cancer.gov/retrieve.cfm?imageid=4537&amp;dpi=72&amp;fileformat=jpg">-->
-  </a>
-  </div>
-  <p>Students who completed science pre-requisites five or more years ago still have an opportunity for admission.  However all courses five or more years and/or with less than a B earned will be required to be repeated.</p>
-<h3>Compare Tracks</h3>
-<p>Be sure to read the full eligibility requirements before applying to the program. Please contact us if you have any question on which track best suits your needs.</p>
+          <h3>Tracks</h3>
 
-  <table class="table">
-<thead>
-  <tr>
-  <th></th>
-<th>Pre-Requisite Sciences* Already Completed</th>
-  <th>Grades in pre-requisite sciences already completed</th>
-  <th>Length of Program</th>
-  </tr>
-  <tr>
-  <td>Career Changer</td>
-  <td>No more than 2</td>
-  <td>Bs or better</td>
-  <td>5 semesters</td>
-  </tr>
-  <tr>
-  <td>ReStart</td>
-  <td>1-3</td>
-  <td>Below B on any course</td>
-  <td>5+ semesters</td>
-  </tr>
-  </table>
-<p>* Pre-requisite Sciences refers to Calculus, Chemistry, Biology, and Physics </p>
-  <center>
-<button class="btn" type="button" onclick="myFunction()">
-  <h3>Have Questions?<br />Want More Information?
-  </button>
+          <p>The UF PHPB program offers two tracks to help you complete pre-requisite coursework. Each track requires full-time enrollment. The track you choose depends on your academic background. All tracks require a bachelor's degree for admission. For more information on each track, see the <a href="admission.php">Admission</a> and <a href="courses.php">Course information</a>.</p>
 
-<script>function myFunction() {    window.open("http://www.clas.ufl.edu/prehealthpostbac/questions.html", "_blank", "toolbar=no, scrollbars=no, resizable=no, location=no, status=no, menubar=no, addressbar=no, titlebar=no, top=500, left=500, width=360, height=525");}
-</script>
+          <p>Students who completed science pre-requisites five or more years ago still have an opportunity for admission.  However all courses five or more years and/or with less than a B earned will be required to be repeated.</p>
 
-</center>
-  <!--<div id="box">
-         <center>
-  <h3>Information Session
-  </center>
-<p>Listen to the <a href="docs/2017PHPBInfoSession.mp4">online Information Session</a> for a better understanding of the program.</p>
-<p>Interested students are invited to attend one of our information sessions to gain a better understanding of the program and have their questions answered. Please go to the Academic Advising Center's <a href="http://www.advising.ufl.edu/postbacworkshops/" target="_blank">Workshop Registration page</a>, select the "Pre-Health" Workshop Area, and register for the session you plan to attend.<br />
-  <strong>Dates: TBA.</strong>
-  </p>
-  </div>-->
-</div>
+          <div class="trackHighlight">
+            <ul>
+              <li>Career Changer Program</li>
+            </ul>
+            <p>For students who have not taken more than two science pre-requisite courses earning Bs or above.</p>
+          </div>
 
-<?php require_once("includes/sidebar.php"); ?>
-<?php require_once("includes/footer.html"); ?>
+          <div class="trackHighlight">
+            <ul>
+              <li>ReStart Program</li>
+            </ul>
+            <p>For students who took three or fewer science pre-requisite courses early in undergrad and earned less than a B without further attempts in science for two or more years.</p>
+          </div>
 
-</body>
+          <h3>Compare Tracks</h3>
+
+          <p>Be sure to read the full eligibility requirements before applying to the program. Please contact us if you have any question on which track best suits your needs.</p>
+
+          <table id="trackTable" class="center">
+            <tr>
+              <th class="italic normalWeight">Requirement</th>
+              <th>Career Changer</th>
+              <th>ReStart</th>
+            </tr>
+            <tr>
+              <td>Completed Pre-Requisite Sciences*</td>
+              <td>2 or fewer</td>
+              <td>1-3</td>
+            </tr>
+            <tr>
+              <td>Grades in pre-requisite sciences already completed</td>
+              <td>Bs or better</td>
+              <td>Below B on any course</td>
+            </tr>
+            <tr>
+              <td>Length of Program</td>
+              <td>Five semesters</td>
+              <td>Five + semesters</td>
+            </tr>
+            <tr>
+              <td colspan="3" class="italic">* Pre-requisite Sciences refers to Calculus, Chemistry, Biology, and Physics</td>
+            </tr>
+          </table>
+
+        <button class="btn" type="button" onclick="Location:'admission.php'">
+          <h3>Have Questions?</h3>
+          <h3>Want More Information?</h3>
+        </button>
+
+      </div>
+    <?php require_once("includes/sidebar.php"); ?>
+    <?php require_once("includes/footer.html"); ?>
+  </body>
 </html>
