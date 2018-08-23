8f6b95bc4f4e213e31cf48e283c632dbeb159862
ahuntercatalano <alex.catalano2@gmail.com>
2018-08-23 09:57:11 -0400

    transfer UFO

--- fixlist/UFO/validator/submit.php
+++ fixlist/UFO/validator/submit.php
@@ -1,9 +1,32 @@
 <?php
+
   //ensure the page was reached via the previous page
   require("include/functions.php");
   if (!isset($_POST['formFour'])) {
     redirectTo("index.php");
   }
+
+  // $dbhost = "localhost";
+  // $dbuser = "root";
+  // $dbpassword = "";
+  // $dbname = "ufovalidator";
+  // $connection = mysqli_connect($dbhost, $dbuser, $dbpassword, $dbname);
+  //
+  // //check if the database is able to connect
+  // if (mysqli_connect_error()) {
+  //   die("Database connection failed" . mysqli_connect_error() . " (" . mysqli_connect_errno() . ")");
+  // }
+  //
+  // //temporary hard coded values to submit to database:
+  // $glid = "4081-1353";
+  // $name = "acatalano2";
+  // $now = date("ymd G:i");
+  //
+  // //insert the variables into the database
+  // $query = "INSERT INTO submissions (glid, Name, now) VALUES ('{$glid}', '{$name}', '{$now}')";
+  // $result = mysqli_query($connection, $query);
+
+
  	$thisPage = "0";
   $submitted = 0;
 	require_once("include/header.php");
--- fixlist/UFO/validator/thankyou.php
+++ fixlist/UFO/validator/thankyou.php
@@ -1,16 +1,5 @@
 <?php
 
-  $dbhost = "localhost";
-  $dbuser = "root";
-  $dbpassword = "";
-  $dbname = "ufovalidator";
-  $connection = mysqli_connect($dbhost, $dbuser, $dbpassword, $dbname);
-
-  //check if the database is able to connect
-  if (mysqli_connect_error()) {
-    die("Database connection failed" . mysqli_connect_error() . " (" . mysqli_connect_errno() . ")");
-  }
-
   //ensure the page was reached via the previous page
   require("include/functions.php");
 
@@ -22,15 +11,6 @@
   $submitted = 1;
 	require_once("include/header.php");
 
-
-  //temporary hard coded values to submit to database:
-  $glid = "4081-1353";
-  $name = "acatalano2";
-  $now = date("ymdGis");
-
-  //insert the variables into the database
-  $query = "INSERT INTO submissions (glid, Name, now) VALUES ('{$glid}', '{$name}', '{$now}')";
-  $result = mysqli_query($connection, $query);
 ?>
 
 
@@ -38,7 +18,7 @@
 
       <p id="greenBorder"><i>Thank you for submitting the form!</i></p>
 
-      <h2><span class="orangeNumber">C</span> Contact: <br><a href="https://www.advising.ufl.edu/uf-online/contact/" target="_blank">CLAS UF Online Advising Staff</a></h2>
+      <h2 style="line-height: 3;"><span class="orangeNumber">C</span> Contact:<br><a href="https://www.advising.ufl.edu/uf-online/contact/" target="_blank">CLAS UF Online Advising Staff</a></p>
 
       <!-- <p><a href="index.php">home</a></p> -->
     </section>
