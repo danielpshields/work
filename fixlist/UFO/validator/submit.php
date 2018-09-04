ea9442eba83fa09aea33c9e45d2efd6bd7419cce
Catalano <acatalano2@ufl.edu>
2018-09-04 17:04:12 -0400

    [ln] open/close tuesday 180904

--- fixlist/UFO/validator/submit.php
+++ fixlist/UFO/validator/submit.php
@@ -1,11 +1,6 @@
 <?php
     // GLID user information
-    $UFID         = $_SERVER['HTTP_UFSHIB_UFID'];
-    $date_auto    = date("Y-m-d H:i:s");
-    $nameBusiness = $_SERVER['HTTP_UFSHIB_BUSINESSNAME'];
-    $email        = $_SERVER['HTTP_UFSHIB_MAIL'];
-    $nameGiven    = $_SERVER['HTTP_UFSHIB_GIVENNAME']." ".$_SERVER['HTTP_UFSHIB_SN'];
-    $nameGivenShort    = $_SERVER['HTTP_UFSHIB_GIVENNAME'];
+
 
     //allows no file to be uploaded
     $yourDocument = "";
--- shell.php
+++ shell.php
@@ -1,9 +1,39 @@
 <?php
+// $iso = 400;
 
-$myArray = array("one", "two", "three");
+$light = int;
+// light    = 1,2,3,4,5,6,7,8,9,10,11,12,13,14;
+$aperture = int;
+// exposure = 1,2,3,4,5,6,7,8;
+$shutterSpeed = int;
+// speed    = 1,2,3,4,5,6,7,8,9,10,11,12;
 
-list($a, $b, $c) = $myArray;
+($light) * ($iso) * ($aperture) = $shutterSpeed;
 
-echo "Printing: $a $b $c";
+// 0. set iso = 400
+
+// 1. set aperture
+
+$apertureInput = int;
+$aperture = $apertureInput;
+    $aperture;
+
+// 2. get light (high, med, low);
+
+  if ($light < 300) (($light >= 301) and ($light < 600)) ($light >= 601) {
+    $light;
+  }
+
+  if ($light is something and $aperture is something) {
+    $shutterSpeed = $math;
+  }
+
+  $math = ($aperture * )
+
+
+  get $light;
+  set $aperture;
+  do $math;
+  echo $shutterSpeed;
 
 ?>
