20294c2624ebe2f912813e42f496bd5c0ceacd59
Catalano <acatalano2@ufl.edu>
2018-09-07 12:18:36 -0400

    [ln] UNOPENED 180907

--- fixlist/PHPB/admission.php
+++ fixlist/PHPB/admission.php
@@ -1,6 +1,7 @@
 <?php
 $title = "Pre-Health Post-Bac Completion Program | UF College of Liberal Arts and Sciences";
 $page = "admission";
+$path = "";
 require_once("include/header.php");
 ?>
 
@@ -8,65 +9,90 @@ require_once("include/header.php");
   <img class="desktopImage" src="image/admission_desktop.jpg" alt="College of Liberal Arts and Sciences Human Resources">
   <img class="mobileImage" src="image/admission.jpg" alt="">
 
-<div class="page">
-  <h2>Admission</h2>
-  <div class="rightColumn">
-    <!-- <h3><b>Fall</b></h3> -->
-    <p>We admit students for the Fall on a rolling basis until the program fills. Students who apply early have a greater chance of admission. We recommend applying by Nov. 1, but will continue to review applications until the deadline if we have space available.</p>
-    <p>This is a highly selective program. Students who apply early have a greater chance of admission. We recommend applying by Nov. 1, but will continue to review applications until the deadline if we have space available.</p>
-    <p>The Admissions Committee reviews each applicant carefully taking into consideration the academic background, previous institutions, courses completed, GPAs earned, evaluations, and essays. The rigor and quality of the applicant's undergraduate curriculum is strongly considered as evidence of having achieved the academic skills necessary for successful completion of this program.</p>
-    <p>Most applicants have completed work or volunteer hours in a clinical setting and demonstrated knowledge of the profession they are pursuing.</p>
-  </div>
-  <div class="clear"></div> <!-- this may be tempory -->
+  <div class="page">
+    <h2>Admission</h2>
+    <!-- <div class="rightColumn"> -->
+    <p><b>Fall admission only.</b></p>
 
-  <section>
-    <p><strong>Applications are reviewed and evaluated on a rolling basis. All application materials must be received for admission to be finalized. The most competitive applicants will be admitted on a space available basis.</strong></p>
-  </section>
-  <div class="rightColumn">
+    <?php include("{$path}include/applyButton.php"); ?>
+
+    <p>This is a highly selective and intense program. Each applicant is carefully reviewed taking into consideration the academic background, previous institutions, courses completed, GPAs earned, evaluations, and essays. The rigor and quality of the applicant's undergraduate and graduate curriculum is strongly considered as evidence of the ability to achieve the academic skills necessary for successful completion of PHPB.</p>
+    <p><b>Applications are reviewed and evaluated on a rolling or first come, first served basis. All application materials must be received for admission to be finalized. The most competitive applicants will be admitted on a space available basis.</b></p>
     <p>Pursuing academic requirements alone is inadequate for application to healthcare professional graduate programs. This program requires students to participate in medically related service and research experiences. These positions are secured by the student. This program has no linkages with professional schools/programs and in no way guarantees admission.</p>
-  </div>
-  <div class="clear">
 
-  </div>
-<h4>Application Requirements:</h4>
-<ul>
-<li>Meet the general requirements and submit an application for post-baccalaureate admission to the University of Florida.</li>
-<li>Have worked or volunteered in a clinical setting or in another service related capacity prior to application.</li>
-</ul>
-<div id="Career Changer">
-<h3>Pre-Health Career Changer Program</h3>
-<p>offers pre-health students an opportunity to take academic pre-requisite courses for admission to medical school, dental school, and other health professional graduate programs.</p>
-<p>This program is specifically for students who did not take science pre-requisite courses as undergraduates (maximum of two completed).</p>
-<h4>Eligibility:</h4>
-<ul>
-<li>Have a recognized baccalaureate degree (or higher) from a regionally accredited college or university, or an international equivalent based on a 4-year curriculum.</li>
-<li>Have a competitive GPA. Most admitted applicants have minimum cumulative and upper-division GPAs of 3.0. Most students who are successful in gaining admission to medical, dental, and veterinary medicine schools have GPAs of 3.5 and higher.</li>
-<li>Have completed MAC1147 and CHM1025 or their equivalents with a minimum grade of B or above prior to admission. Non-UF students may be able to take these courses online through UF prior to beginning the program. Please contact us for more information.</li>
-<li>Show evidence of academic abilities necessary to complete the demands of the pre-health program.</li>
-<li>Students are NOT eligible for admission if they completed three or more of the required science pre-requisite courses in the past five years.</li>
-<li>Have a minimum of 3.0 in any pre-professional science courses already completed.</li>
-</ul>
-</div><!-- end career changers -->
-<div id="restart">
-<h3>Pre-Health ReStart Program</h3>
-<p> offers pre-health students who earned a non-science bachelor's degree an opportunity to take academic pre-requisite courses for admission to medical school or dental school.</p>
-<p>This track is specifically for students who began taking science pre-requisite courses (generally 1-3 courses) as an undergraduate and experienced academic difficulty.</p>
-<p>It is not an academic record enhancing program for students who already completed all of the pre-requisite courses.</p>
-<p> If admitted, you will be expected to repeat any science pre-requisite courses you previously completed (in addition to your remaining pre-requisites) in order to demonstrate mastery of the material. We advise students in this program to continue to demonstrate their academic potential by taking science courses beyond the pre-requisite classes. You will work closely with a pre-health advisor to develop a plan for success.</p>
-<h4>Eligibility:</h4>
-<ul>
-<li>Have a recognized baccalaureate degree (or higher) from a regionally accredited college or university.</li>
-<li>Have a competitive cumulative GPA. Most admitted applicants have minimum cumulative GPAs of 3.0 or higher depending on the profession they are pursuing. Most students who are successful in gaining admission to medical, dental, and veterinary medicine schools have GPAs of 3.5 and higher.</li>
-<li>Be able to articulate the reasons for previous academic difficulty.</li>
-<li>Complete CHM1025 and MAC1147 at UF in the Spring or Summer prior to matriculation in the program. Grades of "B" or better must be earned to meet admissions conditions. Online enrollment may be possible. Please contact us for more information.</li>
-<li>Upon admission students in the ReStart program will have additional Admission Conditions to complete in the summer prior to starting fall semester.  These will be indicated on the letter of acceptance.</li>
-</ul>
-</div><!-- end restart -->
+  </div><!-- page -->
+
+    <div class="listGraphContainer">
+      <h4>Career Changer Program</h4>
+      <p>This program is specifically for students who have not taken science pre-requisite courses (maximum of two completed).</p>
+      <p><b>Eligibility:</b></p>
+      <ul>
+        <li>Baccalaureate degree (or higher) from a regionally accredited college or university,</li>
+        <li>Minimum cumulative and upper-division GPAs of 3.0. Most students who are successful in gaining admission to medical, dental, and veterinary medicine schools have GPAs of 3.5 and higher.</li>
+        <li>MAC1147 Pre-Calculus and CHM1025 Intro to Chemistry or their equivalents with a minimum grade of B or above prior to matriculation. Applicants may take these courses online or in-residence through UF prior to starting PHPB. Contact us for more information.</li>
+        <li>Have a minimum GPA of 3.0 in any pre-professional science courses already completed.</li>
+        <li>Have worked or volunteered in a clinical setting or in another service related capacity prior to application.</li>
+      </ul>
+    </div><!-- list graph container -->
+
+    <div class="listGraphContainer">
+      <h4>Pre-Health ReStart Program</h4>
+      <p>This track is specifically for:</p>
+      <p class="squeezed">Students who took science pre-requisite courses (3 or less) early in undergraduate and earned less than a B and stopped taking science courses. <b><u>No attempts in science courses for a minimum of two years prior to application.</u></b></p>
+
+      <p class="center"><b><u>OR</u></b></p>
+
+      <p class="squeezed">Students who took the pre-requisite science courses greater than five years ago.</p>
+      <p>This is not an academic record enhancing program for students who completed all of the pre-requisite courses.</p>
+      <p><b>All courses five or more years old and/or with less than a B earned are required to be repeated.</b></p>
+      <p><b>Eligibility:</b></p>
+      <ul>
+        <li>Baccalaureate degree (or higher) from a regionally accredited college or university.</li>
+        <li>Cumulative GPA of 3.0 or higher.</li>
+        <li>Be able to articulate the reasons for previous academic difficulty.</li>
+        <li>Complete CHM1025 and MAC1147 <b><u>at UF</u></b> prior to matriculation in the program. Grades of "B" or better must be earned to meet admissions conditions. Online enrollment may be possible. Please contact us for more information.</li>
+        <li>Upon admission students in the ReStart program will have additional Admission Conditions to complete in the summer prior to starting fall semester. These will be indicated on the letter of acceptance.</li>
+      </ul>
+      <p>Students admitted to the ReStart track will be expected to participate in tutoring and other supportive services offered by PHPB.</p>
+
+    </div><!-- list graph container -->
+
+
+  <div class="page">
+    <section>
+
+      <h3>How to Apply:</h3>
+      <p>Follow the steps below to be sure your application is considered complete:</p>
+      <ol>
+        <li><b>Complete the <a href="" target="_blank">PostBacCAS Application</a>, selecting UF as the program and the specific track for which you are applying.</b></li>
+        <li>Complete the <a href="https://www.applyweb.com/uflpostb/index.ftl" target="_blank">University of Florida Post-Baccalaureate Application.</a></li>
+        <p>When filling out the application, please indicate:<br>
+        <b>Type:  “PreHealth Postbac Program<br>
+        College: Liberal Arts and Sciences (UGLAS)<br>
+        Major: LAS PostBac Med School Prereq (LAS_PB4)</b></p>
+        <li>Send official transcripts from each institution you've attended to both PostBacCAS and UF Admissions:<br>
+        University of Florida<br>
+        Office of Admissions<br>
+        PO Box 114000<br>
+        Gainesville, FL 32611-4000</li>
+        <li>Two letters of recommendation are required as part of the PostBacCAS Application. These should be submitted directly to PostBacCAS. Recommenders should be individuals who know you in a professional or academic capacity. They should not be family members, peers, or family friends. Appropriate recommenders include people such as professors, employers, volunteer coordinators, or health care providers you have worked with.</li>
+      </ol>
+
+    </section>
+    <p><b><u>All application materials must be received for admission to be finalized.</u></b></p>
+    <p>Applications are reviewed and evaluated holistically. The Admissions Committee reviews each applicant carefully taking into consideration academic background, previous institutions, courses completed, GPAs earned, letters of recommendation, and essays.</p>
+
+
+  <div id="applicationCycleAnnouncement" class="listGraphContainer">
+    <p>2019</p>
+    <p>Fall Application Cycle via <a href="https://postbaccas.liaisoncas.com/applicant-ux/#/login" target="_blank">PostBacCAS</a></p>
+    <p>Opened August 15, 2018</p>
+  </div><!-- list graph container -->
+
 </div><!-- end Page -->
-</div><!-- Container -->
 
-  <div class="apply">
-    <button onclick="location.href='apply.php';" type="button" name="button">Apply!</button>
-  </div>
+
+
+</div><!-- Container -->
 
 <?php require_once("include/footer.php");?>
--- fixlist/phpb/index.php
+++ fixlist/phpb/index.php
@@ -33,6 +33,9 @@
 
           </div><!-- page -->
 
+          <div class="holdListGraph">
+
+
           <div class="listGraphContainer">
             <h4>Career Changer Program</h4>
             <p>For students who have no previous attempts in pre-requisite science courses. Applicants in the Career Changer track may have up to two science pre-requisite courses with Bs or above.</p>
@@ -43,6 +46,7 @@
             <p>For students who attempted three or fewer science pre-requisite courses early in undergrad and earned less than a B. No further attempts in sciences for two or more years. Additional support services are available for students applying for ReStart.</p>
           </div><!-- list graph container -->
           <div class="clear"></div>
+        </div><!-- hold listGraph -->
 
 
 
@@ -88,10 +92,33 @@
 
         <p><b>Students who completed pre-requisite science courses five or more years ago are welcome to apply.</b></p>
 
-        <p><b><u>All courses five or more years old and/or with less than a B earned are required to be repeated.</u></b></p>
+        <p><b>All courses five or more years old and/or with less than a B earned are required to be repeated.</b></p>
 
-        <p>All application materials must be received for admission to be finalized.
-        Applications are reviewed and evaluated holistically. The Admissions Committee reviews each applicant carefully taking into consideration academic background, previous institutions, courses completed, GPAs earned, letters of recommendation, and essays.</p>
+        <section>
+          
+          <h3>How to Apply:</h3>
+          <p>Follow the steps below to be sure your application is considered complete:</p>
+          <ol>
+            <li><b>Complete the <a href="" target="_blank">PostBacCAS Application</a>, selecting UF as the program and the specific track for which you are applying.</b></li>
+            <li>Complete the <a href="https://www.applyweb.com/uflpostb/index.ftl" target="_blank">University of Florida Post-Baccalaureate Application.</a></li>
+            <p>When filling out the application, please indicate:<br>
+            <b>Type:  “PreHealth Postbac Program<br>
+            College: Liberal Arts and Sciences (UGLAS)<br>
+            Major: LAS PostBac Med School Prereq (LAS_PB4)</b></p>
+            <li>Send official transcripts from each institution you've attended to both PostBacCAS and UF Admissions:<br>
+            University of Florida<br>
+            Office of Admissions<br>
+            PO Box 114000<br>
+            Gainesville, FL 32611-4000</li>
+            <li>Two letters of recommendation are required as part of the PostBacCAS Application. These should be submitted directly to PostBacCAS. Recommenders should be individuals who know you in a professional or academic capacity. They should not be family members, peers, or family friends. Appropriate recommenders include people such as professors, employers, volunteer coordinators, or health care providers you have worked with.</li>
+          </ol>
+
+        </section>
+
+
+        <p><b><u>All application materials must be received for admission to be finalized.</u></b></p>
+
+        <p>Applications are reviewed and evaluated holistically. The Admissions Committee reviews each applicant carefully taking into consideration academic background, previous institutions, courses completed, GPAs earned, letters of recommendation, and essays.</p>
 
       </div><!-- . Page -->
 
