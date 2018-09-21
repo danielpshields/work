6fafbc8deca7823b7195af1091dcf4ec32aa0e76
ahuntercatalano <alex.catalano2@gmail.com>
2018-09-21 14:59:06 -0400

    [ln] open Friday Study JavaScript (180921, 2:59)

---
+++ study/javascript/lessons/02_03/index.php
@@ -0,0 +1,23 @@
+<?php
+  $soft = "turtle";
+?>
+
+<!DOCTYPE html>
+<html lang="en-US">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>An empty page</title>
+</head>
+
+<body>
+
+</body>
+<script>
+
+var date = new Date();
+document.body.innerHTML = "<?php echo $soft; ?>";
+
+</script>
+</html>
---
+++ study/javascript/lessons/03_07/script.js
@@ -0,0 +1,7 @@
+var pens;
+
+pens = ["red", "blue", "green", "orange"];
+
+var fourthPen = pens[3];
+
+console.log("pen four is " + fourthPen);
