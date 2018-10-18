2b7a2c7260f2335a1b62097019c366d65172ff36
ahuntercatalano <alex.catalano2@gmail.com>
2018-10-18 13:11:47 -0400

    [ln] opened thursday (181018, 1:11 p.m.)

--- fixlist/PHPB/archive/holdOld/admission-restart.php
+++
@@ -1,45 +0,0 @@
-<?php
-  $title = "Pre-Health Completion Program | UF College of Liberal Arts and Sciences";
-  $page = "admission";
-  require_once("include/header.php");
-?>
-
-<div class="page">
-<h1>Pre-Health ReStart Program</h1>
-<p>The University of Florida's Pre-Health Post-Bac ReStart Program offers pre-health students who earned a non-science bachelor's degree an opportunity to take academic pre-requisite courses for admission to medical school or dental school. This track is specifically for students who began taking science pre-requisite courses (generally 1-3 courses) as an undergraduate and experienced academic difficulty. It is not an academic record enhancing program for students who already completed all of the pre-requisite courses. If admitted, you will be expected to repeat any science pre-requisite courses you previously completed (in addition to your remaining pre-requisites) in order to demonstrate mastery of the material. We advise students in this program to continue to demonstrate their academic potential by taking science courses beyond the pre-requisite classes. You will work closely with a pre-health advisor to develop a plan for success.</p>
-
-<div id="box">
-<center>
-<h3>Deadline: February 15, 2019</h3>
-</center>
-<p>We admit students for the Fall on a rolling basis until the program fills. Students who apply early have a greater chance of admission. We recommend applying by November 1st, but will continue to review applications until the deadline if we have space available.</p>
-</div>
-<h3>PHPB ReStart Program Eligibility</h3>
-<p>Successful applicants to this highly selective program must:</p>
-<ol>
-  <li>Meet the general requirements and submit an application for post-baccalaureate admission to the University of Florida.</li>
-<li>Have a recognized baccalaureate degree (or higher) from a regionally accredited college or university.</li>
-<li>Have a competitive cumulative GPA. Most admitted applicants have minimum cumulative GPAs of 3.0 or higher depending on the profession they are pursuing. Most students who are successful in gaining admission to medical, dental, and veterinary medicine schools have GPAs of 3.5 and higher.</li>
-<li>Have worked or volunteered in a clinical setting or in another service related capacity prior to application.</li>
-<li>Be able to articulate the reasons for previous academic difficulty.</li>
-<li>Complete CHM1025 and MAC1147 at UF in the Spring or Summer prior to matriculation in the program. Grades of "B" or better must be earned to meet admissions conditions. Online enrollment may be possible. Please contact us for more information.</li>
-<li>Upon admission students in the ReStart program will have additional Admission Conditions to complete in the summer prior to starting fall semester.  These will be indicated on the letter of acceptance.</li>
-</ol>
-<p>The Admissions Committee reviews each applicant carefully taking into consideration the academic background, previous institutions, courses completed, GPAs earned, evaluations, and essays. The rigor and quality of the applicant's undergraduate curriculum is strongly considered as evidence of having achieved the academic skills necessary for successful completion of this program.</p>
-<p>Most applicants have completed work or volunteer hours in a clinical setting and demonstrated knowledge of the profession they are pursuing.</p>
-<p><strong>Applications are reviewed and evaluated on a rolling basis. All application materials must be received for admission to be finalized.  The most competitive applicants will be admitted on a space available basis.</strong></p>
-<p>Pursuing academic requirements alone is inadequate for application to healthcare professional graduate programs. This program requires students to participate in medically related service and research experiences. These positions are secured by the student. This program has no linkages with professional schools/programs and in no way guarantees admission.</p>
-<h3 id="PHCPApplication" >Application</h3>
-<p>Follow the steps below to be sure your application is considered complete:</p>
-<ol>
-  <li><strong>Complete the <a href="https://postbaccas.liaisoncas.com/" target="_blank">PostBacCAS Application</a>, selecting UF as the program for which you are applying.</strong></li>
-<li>Complete the <a href="https://www.applyweb.com/uflpostb/index.ftl" target="_blank">University of Florida Post-Baccalaureate Application</a>.<br />When filling out the application, <strong>be sure to select "PHPB" as your Postbaccalaureate type.</strong></li>
-<li>Send official transcripts from each institution you've attended to:<br />University of Florida<br />Office of Admissions<br />PO Box 114000<br />Gainesville, FL 32611-4000.<br /><strong>Note: You must also send official transcripts directly to PostBacCAS.</strong></li>
-<li>Two letters of recommendation are required as part of the PostBacCAS Application. These should be submitted directly to PostBacCAS. Recommenders should be individuals who know you in a professional or academic capacity. They should not be family members, peers, or family friends. Appropriate recommenders include people such as professors, employers, volunteer coordinators, or health care providers you have worked with.</li>
-</ol>
-</div>
-
-<?php require_once("include/sidebar.php"); ?>
-   <?php require_once("include/footer.php"); ?>
-  </body>
-  </html>
--- fixlist/PHPB/archive/holdOld/courses-change.php
+++
@@ -1,204 +0,0 @@
-<?php
-  $title = "Pre-Health Post-Bac Career Changer | UF College of Liberal Arts and Sciences";
-  $page = "courses";
-  require_once("include/header.php");
-?>
-
-<div class="page">
-  <!-- <h1>Career Changer Track Course Information</h1>
-  <p>Students in the post-bac program will follow a defined schedule of courses. Cross-disciplinary labs ("x-labs") engage students in a unique interdisciplinary curriculum. These laboratories are equivalent to completing two semesters of General Chemistry labs, Biology labs, and Physics labs, thus meeting the requirements of professional schools.</p>
-  <h3>Course Plan</h3>
-  <br />
-<p><strong>Pre-Matriculation</strong>: MAC1147 and CHM1025 with Bs or above.</p> -->
-
-  <table class="table">
-    <tr>
-      <th>Term</th>
-      <th>Course</th>
-      <th>Title</th>
-      <th>Credits</th>
-    </tr>
-    <tr>
-      <td rowspan="5">Fall 1</td>
-      <td>CHM2045</td>
-      <td>Gen. Chemistry 1</td>
-      <td>3</td>
-    </tr>
-    <tr>
-      <td>BSC2010</td>
-      <td>Principles of Biology 1</td>
-      <td>3</td>
-    </tr>
-    <tr>
-      <td>MAC2311</td>
-      <td>Analytic Geometry &amp; Calculus 1</td>
-      <td>4</td>
-    </tr>
-    <tr>
-      <td>PSY2012</td>
-      <td>Psychology</td>
-      <td>3</td>
-    </tr>
-    <tr>
-      <td>IDS4930</td>
-      <td>Pre-Health Seminar</td>
-      <td>2</td>
-    </tr>
-    <tr>
-      <td colspan="3">Total Semester Hours:</td>
-      <td>15</td>
-    </tr>
-    <tr>
-      <th>Term</th>
-      <th>Course</th>
-      <th>Title</th>
-      <th>Credits</th>
-    </tr>
-    <tr>
-      <td rowspan="5">Spring 1</td>
-      <td>CHM2046</td>
-      <td>Gen. Chemistry 2</td>
-      <td>3</td>
-    </tr>
-    <tr>
-      <td>BSC2011</td>
-      <td>Principles of Biology 2</td>
-      <td>3</td>
-    </tr>
-    <tr>
-      <td>STA2023</td>
-      <td>Statistics</td>
-      <td>3</td>
-    </tr>
-    <tr>
-      <td>SYG2000</td>
-      <td>Principles of Sociology</td>
-      <td>3</td>
-    </tr>
-    <tr>
-      <td>ISC2400L</td>
-      <td>X-Lab 1</td>
-      <td>3</td>
-    </tr>
-    <tr>
-      <td colspan="3">Total Semester Hours:</td>
-      <td>15</td>
-    </tr>
-    <tr>
-      <th>Term</th>
-      <th>Course</th>
-      <th>Title</th>
-      <th>Credits</th>
-    </tr>
-    <tr>
-      <td rowspan="2">Summer 1</td>
-      <td>CHM2210</td>
-      <td>Organic Chemistry 1</td>
-      <td>3</td>
-    </tr>
-    <tr>
-      <td>PHY2053</td>
-      <td>Physics 1</td>
-      <td>4</td>
-    </tr>
-    <tr>
-      <td colspan="3">Total Semester Hours:</td>
-      <td>7</td>
-    </tr>
-
-    <tr>
-      <th>Term</th>
-      <th>Course</th>
-      <th>Title</th>
-      <th>Credits</th>
-    </tr>
-
-    <tr>
-      <td rowspan="5">Fall 2</td>
-      <td>CHM2211</td>
-      <td>Organic Chemistry 2</td>
-      <td>3</td>
-    </tr>
-    <tr>
-      <td>PHY2054</td>
-      <td>Physics 2</td>
-      <td>4</td>
-    </tr>
-    <tr>
-      <td>ISC2401L</td>
-      <td>X-Lab 2</td>
-      <td>3</td>
-    </tr>
-    <tr>
-      <td>IDS4930</td>
-      <td>Innovations in Healthcare</td>
-      <td>3</td>
-    </tr>
-    <tr>
-      <td>MDU4061</td>
-      <td>Bioethics</td>
-      <td>3</td>
-    </tr>
-    <tr>
-      <td colspan="3">Total Semester Hours:</td>
-      <td>16</td>
-    </tr>
-    <tr>
-      <th>Term</th>
-      <th>Course</th>
-      <th>Title</th>
-      <th>Credits</th>
-    </tr>
-    <tr>
-      <td rowspan="5">Spring 2</td>
-      <td>BCH4024 or CHM3218</td>
-      <td>Biochemistry</td>
-      <td>4</td>
-    </tr>
-    <tr>
-      <td>PCB3063</td>
-      <td>Genetics</td>
-      <td>4</td>
-    </tr>
-    <tr>
-      <td>CHM2211L</td>
-      <td>Organic Chemistry Lab</td>
-      <td>2</td>
-    </tr>
-    <tr>
-      <td>IDS4930</td>
-      <td>Health Equity Here &amp; Abroad</td>
-      <td>3</td>
-    </tr>
-    <tr>
-    <td>Elective</td>
-      <td>PSY/SY Elective</td>
-      <td>3</td>
-    </tr>
-    <tr>
-      <td colspan="3">Total Semester Hours:</td>
-      <td>16</td>
-    </tr>
-    <tr>
-      <td colspan="3"><strong>Total Credit Hours:</strong></td>
-      <td>69</td>
-    </tr>
-</table>
-
-
-<ul>
-  <li>Pre-dent and pre-vet students will need MCB3020/L. This course may be taken after completion of PHPB as a non-degree seeking student. There is no guarantee this course can be taken at UF due to space availability. Students interested in taking the course at UF should contact the Microbiology department.</li>
-</ul>
-
-<p>
-  <strong>Possible Electives</strong><br />Most courses in the College of Liberal Arts &amp; Sciences are available. A few suggestions:<br />BSC3096 Human Physiology<br />CLP3144 Abnormal Psychology<br />DEP3053 Developmental Psychology<br />PCB3023 Essential Cell Biology<br />PCB4723C Physiology &amp; Molecular Biology of Animals<br />ZOO3603c Evolutionary Developmental Biology<br />ZOO3713C Functional Vertebrate Anatomy<br />SYO4400 Medical Sociology<br />PSB3340 Behavioral Neuroscience<br />ANT3520 Skeleton Key Forensic<br /><br /><strong>Courses offered by the UF College of Medicine:</strong><br />MDU4003 Intro. to the Professions of Medicine<br />MDU4004 Physician Shadowing<br />MDU4031 Medicine and the Law<br />MDU4061 Intro to Medical Bioethics<br />MDU4850 Diseases of Eating</p>
-<p>Electives could also include other <a href="http://advising.ufl.edu/prehealth/handouts/PreHealthElectives.pdf">non-science courses</a> that will enhance students' knowledge of working with people and groups.</p>
-
-
-</div>
-
-    <?php require_once("include/sidebar.php"); ?>
-   <?php require_once("include/footer.php"); ?>
-
-  </body>
-</html>
