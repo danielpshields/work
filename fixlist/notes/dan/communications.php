23c7f36ec76ec2f20b15cf1ee87efc98e981470e
ahuntercatalano <alex.catalano2@gmail.com>
2018-10-24 11:45:15 -0400

    [ln] lunch

---
+++ fixlist/notes/dan/communications.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>181024_dan_communications</h1>
+    <ul>
+      <li><a href="#">Manual</a>: <a href="#">projects</a></li>
+    </ul>
+    <?php
+
+      $cc = array['alex', 'dan'];
+
+      $cc => $roles = array[$secretarial; $developer; $chair;]
+
+    ?>
+  </body>
+</html>
---
+++ fixlist/notes/lynn/general.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>181024_lynn</h1>
+
+    <p><i>Lynn is having trouble with using the numlock on her computer</i></p>
+
+    <ul>
+      <li>1:670 advisor to student ratio</li>
+    </ul>
+
+    <h2>LT:</h2>
+    <ol>
+      <li>one</li>
+      <ul>
+        <li>peer advising undergrads</li>
+        <li>GAs are taking two months</li>
+        <li><i>divert simpler things</i></li>
+        <li>*drew a table with eight th columns and zero td</li>
+      </ul>
+      <li>two</li>
+      <ul>
+        <li>we're responsible for dual role, we're not funded resources for what we do</li>
+      </ul>
+    </ol>
+
+    <ul>
+      <li>students want to see a person, in person</li>
+      <li>different advisors have different talents, interests (but they all tend to follow the same quality > quantity philosophy)</li>
+    </ul>
+
+
+
+
+  </body>
+</html>
