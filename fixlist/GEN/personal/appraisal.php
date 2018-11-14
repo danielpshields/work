73c464cce76c5af653ed4a46d713625557ce3cb5
ahuntercatalano <alex.catalano2@gmail.com>
2018-11-14 16:56:27 -0500

    [ln] close Wednesday (181114, 4:56 p.m.)

---
+++ fixlist/GEN/personal/appraisal.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <ul>
+      <li><a href="https://docs.google.com/document/d/1G7LU0Bla7Qop6UB4-RDi2QAMW2Ozl4yGBDNadgQKJ-o/edit?usp=sharing" target="_blank">Spit Here Google Doc</a></li>
+    </ul>
+    <ul>
+      <li>productivity: What I've done since my hire...</li>
+      <li>initiative: Who I've connected with...</li>
+      <li>leadership: When I've voiced myself</li>
+      <li>some goals for the next period.</li>
+    </ul>
+    <ul>
+      <li>principles</li>
+      <li>standards</li>
+      <li>guidelines</li>
+    </ul>
+  </body>
+</html>
