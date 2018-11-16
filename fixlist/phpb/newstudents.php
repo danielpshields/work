ca6db5b6e2e9f54de9c336b8348d81ee0523405b
ahuntercatalano <alex.catalano2@gmail.com>
2018-11-16 11:39:08 -0500

    [GEN, appraisal letter] (11:38 a.m.)

--- fixlist/phpb/newstudents.php
+++ fixlist/phpb/newstudents.php
@@ -5,40 +5,67 @@
   require_once("include/header.php");
 ?>
 
+  <div class="shell">
+
+    <h2>Newly Admitted Students</h2>
+    <h3>Welcome.</h3>
+    <p>Congratulations on your admission to the University of Florida's Pre-Health Postbaccalaureate Program (PHPB)!</p>
+    <p>You are about to start an intense program with others pursuing similar goals as your own. You will be in small classes with just those in the PHPB program for your sciences and math.</p>
+    <h3>A few bits of information:</h3>
+    <p>Some of you were admitted with the requirement that you complete Pre-Calculus (MAC1147) and Intro to Chemistry (CHM1025). If you were admitted to the <u>ReStart</u> program these <u>must</u> be taken at UF and completed with grades of B or above. Both courses are available online and in person.</p>
+    <p>If you were admitted to the "<u>Career Changer</u>" track you are encouraged to take these at UF, but they may be taken at any institution. These must be completed with grades of B or above. Course equivalents must be pre-approved by the PHPB Director.</p>
+
+    <p>To take courses at UF:</p>
+    <ul>
+
+      <li>Please contact the <a href="mailto:phpostbac@clas.ufl.edu">PHPB Director</a> for this process.</li>
+
+      <li>Registration is during drop/add, usually the second day of classes. You will register for these courses yourself, but please contact the PHPB Director if you have any difficulty. There are no guarantees that seats will be available.</li>
+
+      <li>If you are taking these at another institution, please have the courses pre-approved by the PHPB Director for equivalency prior to taking these courses. Upon completion send an official transcript to the UF Registrar and an unofficial copy of your grades sent to the PBPB director immediately upon completion. These courses must be completed with a grade prior to beginning PHPB in August.</li>
+
+      <li>You may take MAC1147 via <a href="http://flexible.dce.ufl.edu/" target="_blank">Flexible Learning</a> which is an online option with flexible start dates. Please go to: <a href="http://flexible.dce.ufl.edu/" target="_blank">http://flexible.dce.ufl.edu/</a> for registration information.</li>
+    </ul>
 
 
-<div class="shell">
-
-  <h2>Newly Admitted Students</h2>
-  <h3>Welcome.</h3>
-  <p>Congratulations on your admission to the University of Florida's Pre-Health Postbaccalaureate Program (PHPB)!</p>
-  <p>You are about to start an intense program with others pursuing similar goals as your own. You will be in small classes with just those in the PHPB program for your sciences and math.</p>
-  <h3>A few bits of information:</h3>
-  <p>Some of you were admitted with the requirement that you complete Pre-Calculus (MAC1147) and Intro to Chemistry (CHM1025). If you were admitted to the ReStart program these must be taken at UF and completed with grades of B or above. Both courses are available online and in person.</p>
-  <p>If you were admitted to the "Career Changer" track you are encouraged to take these at UF, but they may be taken at any institution. These must be completed with grades of B or above. Course equivalents must be pre-approved by the PHPB Director.</p>
-</div><!-- shell -->
-<div class="clear"></div>
+  </div><!-- shell -->
+  <div class="clear"></div>
 
   <p>If you will not be in Gainesville during the summer, these courses are typically offered online. To take them at UF:</p>
-  <ul listtype="alpha">
+
+  <ul>
+
     <li>You will need to complete a "non-degree" seeking application through <a href="http://www.admissions.ufl.edu/apply/more">Admissions</a>. Registration is during drop/add, usually the second day of classes. You will register for these yourself, but please contact us if you have any difficulty. There are no guarantees that seats will be available.</li>
+
     <li>If you are taking these at another institution, please have an official transcript sent to UF Registrar and an unofficial copy of your grades sent to the PBPB coordinator as soon as possible. You cannot begin fall courses in August without submitting final grades. </li>
+
     <li>You may also take MAC1147 via <a href="http://flexible.dce.ufl.edu/">Flexible Learning</a> which is an online option with flexible start dates. Please see their website for registration information.</li>
+
   </ul>
 
   <h3>Also:</h3>
 
-  <!-- <img src="images/workshops.jpg" class="workshops" alt="Students Studying" title="Students Studying"/> -->
+
   <ol>
-    <li>Please be certain to establish your Gatorlink account by reading the <a href="http://identity.it.ufl.edu/process/gatorlink/create-account/" target="_blank">instructions</a>. </li>
+    <li>Please be certain to establish your Gatorlink account by reading the <a href="http://identity.it.ufl.edu/process/gatorlink/create-account/" target="_blank">instructions</a>.</li>
+
     <li><strong>An academic plan will be sent to you individually</strong>. Please review it, sign it and return it to the PHPB Coordinator via email within five days of receipt.</li>
-    <li>Complete your FAFSA for UF Student Financial Aid (SFA).  There is possible access to federal loans <strong>during the first year only</strong>.  These do not cover full tuition.  Any remaining tuition is out of pocket or private loans.  Speak with UF SFA about your needs.  You will need to tell them you are coded as LS-IDS-HPB and you are a post-bac student completing the pre-requisite courses for admission to professional graduate school. Once the cohort is complete the PHPB Coordinator will be sending SFA a final list of everyone in the cohort so they will have the information. </li>
-    <li>Each semester you will be registered for your courses by the PHPB Program Coordinator. Your courses will be in section separate from undergraduate students and you do not need to be concerned about seat availability. </li><li>Classes begin August 22, 2018. See "<a href="https://catalog.ufl.edu/ugrad/current/Pages/dates-and-deadlines.aspx">Dates and Deadlines</a>" for more information. </li>
+
+    <li>Complete your FAFSA for UF Student Financial Aid (SFA). There is possible access to federal loans <strong>during the first year only</strong>. These do not cover full tuition. Any remaining tuition is out of pocket or private loans.  Speak with UF SFA about your needs.  You will need to tell them you are coded as LS-IDS-HPB and you are a post-bac student completing the pre-requisite courses for admission to professional graduate school. Once the cohort is complete the PHPB Coordinator will be sending SFA a final list of everyone in the cohort so they will have the information.</li>
+
+    <li>Each semester you will be registered for your courses by the PHPB Program Coordinator. Your courses will be in section separate from undergraduate students and you do not need to be concerned about seat availability.</li>
+
+  <li>Classes begin August 20, 2019. See "<a href="https://catalog.ufl.edu/UGRD/dates-deadlines/" target="_blank">Dates and Deadlines</a>" for more information.</li>
+
     <li>HOLDS:  upon admission and each semester you will have holds placed on your records.  Go to <a href="https://student.ufl.edu/">https://student.ufl.edu/</a> and under the "Holds" link read the corresponding text and get these removed.  If you get a hold for "Mandatory Insurance" please let the PHPB Coordinator know.  As PHPB you are exempt from this requirement.  However if you need health insurance you may opt to accept this and the cost will be included with your tuition bill.  When you get your tuition invoice please be sure to review it carefully to make sure this charge is accurate for you specifically.  There will always be a "LS REGISTRATION ADJUST" hold on your records.  I place that holds while you are in PHPB.  Don't worry about that hold.We will have an orientation on the first day of classes. <strong>This is mandatory</strong>. Not attending orientation will result in not matriculating to PHPB. Your classes will start in the afternoon.</li>
+
     <li>There is also an <a href="https://lss.at.ufl.edu/">online PHPB Orientation</a> with information regarding multiple topics. Click on the Canvas link. Sign in using your Gatorlink username and password and then look under the "Courses" link at the top of the page. Click on PHPB Orientation. Be sure to review the information under the "Assignment" link. Be certain you are familiar with the PHPB expectations.</li>
-    <li>PHPB is rigorous and intense. We want students to come prepared. In the PHPB Orientation information on e-learning there is an assignment that ALL PHPB students need to complete. <strong>Please complete the VARK.</strong> The information on how to access this is available to you via the assignment link. ONCE YOU HAVE COMPLETED THE VARK, BRING THE RESULTS WITH YOU TO IDS4930 PHPB SEMINAR.<br />The information on how to access these is available to you via the assignment link. ONCE YOU HAVE COMPLETED THE VARK, BRING THE RESULTS WITH YOU TO THE PHPB SEMINAR, IDS4930, about which you will receive information in August.</li>
+
+    <li>PHPB is rigorous and intense. We want students to come prepared. In the PHPB Orientation information on e-learning there is an assignment that ALL PHPB students need to complete. <strong>Please complete the VARK.</strong> The information on how to access this is available to you via the assignment link. ONCE YOU HAVE COMPLETED THE VARK, BRING THE RESULTS WITH YOU TO IDS4930 PHPB SEMINAR.<br>The information on how to access these is available to you via the assignment link. ONCE YOU HAVE COMPLETED THE VARK, BRING THE RESULTS WITH YOU TO THE PHPB SEMINAR, IDS4930, about which you will receive information in August.</li>
+
     <li>To prepare for starting PHPB, please review the workshops and resources at the <a href="https://teachingcenter.ufl.edu/study-skills/video-resources/">UF Teaching Center</a>. THIS WILL CONTRIBUTE TO YOUR SUCCESS AT UF.</li>
-    <li>One of the expectations of PHPB is that students will be engaged in clinical volunteering and community service while in the program.  This fills quickly.  I recommend you start looking for these for fall now.  View the <a href="https://www.advising.ufl.edu/pre-health/pre-health-resources/">Pre-Health Resources</a> to begin looking for places in Gainesville to do this.  We will talk more about this during orientation.</li>
+
+    <li>One of the expectations of PHPB is that students will be engaged in clinical volunteering and community service while in the program. This fills quickly. I recommend you start looking for these for fall now. View the <a href="current.php#additionalResources">Pre-Health Resources</a> to begin looking for places in Gainesville to do this. We will talk more about this during orientation. Information regarding orientation will be sent to you.</li>
   </ol>
 
 <?php  require_once("include/footer.php");?>
