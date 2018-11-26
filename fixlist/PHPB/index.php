d3bc7b52bbe2f6b375e2a041629cac0429f2d65a
ahuntercatalano <alex.catalano2@gmail.com>
2018-11-26 16:56:58 -0500

    [ln] close Monday (181126, 4:56 p.m.)

--- fixlist/PHPB/index.php
+++ fixlist/PHPB/index.php
@@ -22,10 +22,16 @@
 
       <div class="quoteBubble">
 
-        <div class="quote_icon_container">
-          <?php echo $open; ?>
-        </div>
+        <!-- <div class="quote_icon_container">
+          <?php //echo $open; ?>
+        </div> -->
         <!-- quote icon container -->
+
+        <div class="mobile_quote_icon">
+          <?php echo $open; ?>
+        </div><!-- mobile quote icon -->
+
+
 
         <p class="quoteSlug">As a Career Changer, the PHPB Program saved me precious time and resources. Instead of pursuing a second four-year Bachelors degree, the PHPB Program's curriculum allowed me to complete the necessary pre-requisites in two years. I can confidently say the program's rigor prepared me for the academic challenges of dental school. More importantly, the individual advising and mentorship I received helped me become a more desirable, well-rounded, and competitive applicant. Thank you PHPB for playing an instrumental role in helping me achieve my goal of attending the University of Florida College of Dentistry!</p>
 
@@ -33,9 +39,10 @@
           <?php echo $quote_close; ?>
         </div><!-- quote icon container -->
 
+        <p class="quote_signature">Karley Post<br>2014 PHPB Cohort, University of Florida, College of Dentistry</p>
+
       </div><!-- quote bubble -->
 
-      <p>Karley Post, 2014 PHPB Cohort, University of Florida, College of Dentistry</p>
 
       <p>Students considering all health professions are welcome. However, for professions other than medicine, some pre-requisite courses are not included in our standard curriculum. These courses are taken after completion of the PHPB program.</p>
 
