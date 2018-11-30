58dcfdef1645f3300b6ffdb6890649bafcfab84b
alex catalano <alex.catalano2@gmail.com>
2018-11-29 21:06:10 -0500

    [notes] whiteboard meeting notes

--- fixlist/GEN/notes/dan/phpb.php
+++ fixlist/GEN/notes/dan/phpb.php
@@ -1,23 +1,6 @@
 A-man, here are some thoughts on the PHPB site:
 
-•	Very nice css!
-•	Is all the content updated and accurate? Images?
-•	The CLAS logo in top left should go to clas.ufl.edu.
-•	The top right UF logo should go to ufl.edu.
-•	The title tags need to be updated.
-•	There’s no favicon.
-•	Home page: listen to the video at 25 seconds. Sounds like a large gulp?
-•	Home page: should the 4 entries on the bottom link to applicable pages?
-•	The contact page image is grainy.
-•	Alumni success page image is blurry.
-•	Advising page has an unlinked email: https://www.advising.ufl.edu/ahc/PHPB/advising.php.
-•	The phone numbers in left nav and content can be linked to tel:
-•	The Twitter and FB logos in left nav look left aligned. Should they be centered?
-•	Is there anything about the site management that should be added to our task calendar?
-
-I’m sure I’m missing something. 
-
-- http://identity.ufl.edu/web/
-
-- fonts
-- web-specific colors
+The title tags need to be updated.
+Image resized?
+Is there anything about the site management that should be added to our task calendar?
+fonts
---
+++ fixlist/GEN/notes/whiteboard_meeting_notes/meeting_images.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>meeting images</title>
+    <link rel="stylesheet" href="css/master.css">
+  </head>
+  <body>
+    <h1>Whiteboard</h1>
+    <img src="image/whiteboard.gif" alt="">
+    <h2>1</h2>
+    <img src="image/01_01.jpg" alt="">
+    <img src="image/01_02.jpg" alt="">
+    <img src="image/01_03.jpg" alt="">
+    <h2>2</h2>
+    <img src="image/02.jpg" alt="">
+    <h2>3</h2>
+    <img src="image/03.jpg" alt="">
+
+  </body>
+</html>
