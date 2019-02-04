e0ecf2fbcad51653ee49d58e818f2bb39c50343d
ahuntercatalano <alex.catalano2@gmail.com>
2019-02-04 13:25:03 -0500

    [ln] open Monday (190204 1:24 p.m.)

---
+++ ln/time.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="css/time.css">
+  </head>
+  <body>
+
+    <table>
+      <tr>
+        <th>date</th>
+        <th>in</th>
+        <th>out</th>
+        <th>in</th>
+        <th>out</th>
+        <th>current</th>
+      </tr>
+      <tr>
+        <td><?php echo date('l M. j'); ?></td>
+        <td>7:30 a.m.</td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td>5</td>
+      </tr>
+    </table>
+
+  </body>
+</html>
