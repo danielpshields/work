18ca3df1a8f08493977410f6260116a90efd02eb
ahuntercatalano <alex.catalano2@gmail.com>
2019-02-06 10:53:34 -0500

    cat and staff meetings

---
+++ ln/meeting/staff.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <table>
+      <tr>
+        <td>February</td>
+        <td>6</td>
+      </tr>
+      <tr>
+        <td>February</td>
+        <td>20</td>
+      </tr>
+      <tr>
+        <td>February</td>
+        <td>27</td>
+      </tr>
+      <tr>
+        <td>March</td>
+        <td>13</td>
+      </tr>
+      <tr>
+        <td>March</td>
+        <td>20</td>
+      </tr>
+      <tr>
+        <td>April</td>
+        <td>24</td>
+      </tr>
+
+    </table>
+  </body>
+</html>
