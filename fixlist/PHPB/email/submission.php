c34cb7d00885942faffe88f82159621d1d440c35
alex catalano <alex.catalano2@gmail.com>
2019-02-06 20:07:14 -0500

    [phpb form] if statements to display !empty

--- fixlist/PHPB/email/submission.php
+++ fixlist/PHPB/email/submission.php
@@ -1,52 +1,56 @@
+<div class="container">
+
 
   <h1>ReStart Application Submission</h1>
   <p>Submitted: <?php echo $date = "Feb. 6"; ?></p>
+
+
+  <div id="flexy">
+
 
   <!-- name -->
   <table>
     <thead>
       <tr>
-        <th>Name</th>
+        <th>Applicant Name</th>
       </tr>
     </thead>
     <tbody>
       <tr>
-        <td><?php echo $name; ?></td>
+        <td><?php echo ucfirst($name); ?></td>
       </tr>
     </tbody>
   </table>
 
-  <!-- area of study -->
-  <table>
-    <thead>
-      <tr>
-        <th>Area of Study</th>
-      </tr>
-    </thead>
-    <?php
 
-      $areaOfStudy = array(
-        $studied_preDental,
-        $studied_preMedical,
-        $studied_preVeterinary
-      );
-
-    ?>
-    <tbody>
-      <tr>
-        <?php foreach ($areaOfStudy as $key) {
-          if (!empty($key)) {
-        ?>
+  <?php
+    if (!empty($studied_preVeterinary) || !empty($studied_preMedical) || !empty($studied_preDental)) { ?>
+    <table>
+      <thead>
+        <tr>
+          <th>Previous Area(s) of Study</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php
+          $areaOfStudy = array(
+            $studied_preDental,
+            $studied_preMedical,
+            $studied_preVeterinary
+          );
+          foreach($areaOfStudy as $key) {
+            if(!empty($key)) { ?>
+        <tr>
           <td><?php echo $key; ?></td>
-        <?php } else {
-          echo $studied_other;
-        }
-      } ?>
-      </tr>
-    </tbody>
-  </table>
+        </tr>
+        <?php }
+       } ?>
+      </tbody>
+    </table>
+    <?php } ?>
 
-  <!--  -->
+
+  <!--
   <table>
     <thead>
       <tr>
@@ -59,241 +63,8 @@
       </tr>
     </tbody>
   </table>
+-->
 
-  <!--  -->
-  <table>
-    <thead>
-      <tr>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td></td>
-      </tr>
-    </tbody>
-  </table>
+</div><!-- flexy -->
 
-  <!--  -->
-  <table>
-    <thead>
-      <tr>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td></td>
-      </tr>
-    </tbody>
-  </table>
-
-  <!--  -->
-  <table>
-    <thead>
-      <tr>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td></td>
-      </tr>
-    </tbody>
-  </table>
-
-  <!--  -->
-  <table>
-    <thead>
-      <tr>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td></td>
-      </tr>
-    </tbody>
-  </table>
-
-  <!--  -->
-  <table>
-    <thead>
-      <tr>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td></td>
-      </tr>
-    </tbody>
-  </table>
-
-  <!--  -->
-  <table>
-    <thead>
-      <tr>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td></td>
-      </tr>
-    </tbody>
-  </table>
-
-  <!--  -->
-  <table>
-    <thead>
-      <tr>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td></td>
-      </tr>
-    </tbody>
-  </table>
-
-  <!--  -->
-  <table>
-    <thead>
-      <tr>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td></td>
-      </tr>
-    </tbody>
-  </table>
-
-  <!--  -->
-  <table>
-    <thead>
-      <tr>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td></td>
-      </tr>
-    </tbody>
-  </table>
-
-  <!--  -->
-  <table>
-    <thead>
-      <tr>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td></td>
-      </tr>
-    </tbody>
-  </table>
-
-  <!--  -->
-  <table>
-    <thead>
-      <tr>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td></td>
-      </tr>
-    </tbody>
-  </table>
-
-  <!--  -->
-  <table>
-    <thead>
-      <tr>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td></td>
-      </tr>
-    </tbody>
-  </table>
-
-  <!--  -->
-  <table>
-    <thead>
-      <tr>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td></td>
-      </tr>
-    </tbody>
-  </table>
-
-  <!--  -->
-  <table>
-    <thead>
-      <tr>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td></td>
-      </tr>
-    </tbody>
-  </table>
-
-  <!--  -->
-  <table>
-    <thead>
-      <tr>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td></td>
-      </tr>
-    </tbody>
-  </table>
-
-  <!--  -->
-  <table>
-    <thead>
-      <tr>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td></td>
-      </tr>
-    </tbody>
-  </table>
-
-  <!--  -->
-  <table>
-    <thead>
-      <tr>
-        <th></th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td></td>
-      </tr>
-    </tbody>
-  </table>
+</div><!-- container -->
