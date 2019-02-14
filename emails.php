24ea287ef079fcfec2abe0eeaba378a1b3d74856
ahuntercatalano <alex.catalano2@gmail.com>
2019-02-14 17:12:17 -0500

    [ln] close Thursday (190214, 5:11 p.m.)

---
+++ emails.php
@@ -0,0 +1,47 @@
+<?php
+  $path  = "";
+  $title = "email";
+  $stylesheet = $title;
+  include("include/header.php");
+?>
+
+<h2>Emails</h2>
+
+  <table>
+    <thead>
+      <tr>
+        <th>host</th>
+        <th>email</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>gmail</td>
+        <td>alex.catalano2@gmail.com</td>
+      </tr>
+      <tr>
+        <td>forwardsGmail</td>
+        <td>alex@alexcatalano.com</td>
+      </tr>
+      <tr>
+        <td>alexcatalano</td>
+        <td>info@alexcatalano.com</td>
+      </tr>
+      <tr>
+        <td>icloud</td>
+        <td>alexcatalano@icloud.com</td>
+      </tr>
+      <tr>
+        <td>UFL</td>
+        <td>acatalano2@ufl.edu</td>
+      </tr>
+      <tr>
+        <td>UFL</td>
+        <td>acatalano@advising.ufl.edu</td>
+      </tr>
+    </tbody>
+  </table>
+
+
+
+<?php include("include/footer.php"); ?>
---
+++ fixlist/UFO/shell/index.php
@@ -0,0 +1,6 @@
+<?php
+
+header("Location: http://alexcatalano.com/PaCE/dashboard.php");
+exit;
+
+?>
