0149444178f7ee0507004a5d1c8d5c6876c8c837
ahuntercatalano <alex.catalano2@gmail.com>
2019-02-19 17:00:22 -0500

    [ln] close Tuesday (190219, 5 p.m.)

---
+++ fixlist/GEN/report/cc.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <ul>
+      <li><a href="cc.php">cc.php</a></li>
+      <li><a href="web.php">web.php</a></li>
+    </ul>
+    <h2>Report</h2>
+    <ul>
+      <li>code review:</li>
+      <ul>
+        <li>pushing back announcement to next week</li>
+        <li>screenshots: mostly about redundancies</li>
+        <ul>
+          <li>eventually get to the schematic layout</li>
+        </ul>
+        <li>ikea project</li>
+      </ul>
+      <li>general:</li>
+      <ul>
+        <li>a lot of logic</li>
+        <li>been somewhat ignoring PHPB</li>
+      </ul>
+      <li>propose:</li>
+      <ul>
+        <li>Uline Project</li>
+      </ul>
+    </ul>
+
+
+
+
+  </body>
+</html>
---
+++ fixlist/GEN/report/web.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <ul>
+      <li><a href="cc.php">cc.php</a></li>
+      <li><a href="web.php">web.php</a></li>
+    </ul>
+  </body>
+</html>
