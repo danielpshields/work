2d044b415f23cf6eb74f15dc28c674a407cda2c3
ahuntercatalano <alex.catalano2@gmail.com>
2019-02-21 17:12:20 -0500

    [ln] close Thursday (190221, 5:11 p.m.)

---
+++ fixlist/GEN/projects/index.php
@@ -0,0 +1,7 @@
+<h3>Project</h3>
+<ul>
+  <li><a href="">Ikea | happy or not</a></li>
+  <li><a href="https://dispenser.short-edition.com/" target="_blank">Harvard | short story machine</a></li>
+  <li>https://dispenser.short-edition.com/</li>
+  <li>Uline | catalog comb</li>
+</ul>
