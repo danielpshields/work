90e810b9dfca75e8749b0420f8219ecda6297d68
alex catalano <alex.catalano2@gmail.com>
2019-02-21 21:58:25 -0500

    [notes] nicole, links

---
+++ fixlist/GEN/notes/nicole/index.php
@@ -0,0 +1,4 @@
+<ul>
+  <li>https://www.reddit.com/r/dataisbeautiful/comments/as3si5/when_were_the_best_films_ever_released_depends/</li>
+  <li>https://www.reddit.com/r/funny/comments/at6hnv/i_solved_this_maze_on_the_first_try/</li>
+</ul>
