d31709e049923cd2c1914bc4de6c444e9f409490
alex catalano <alex.catalano2@gmail.com>
2019-02-25 00:11:41 -0500

    [notes, nicole] [images]

--- fixlist/GEN/notes/nicole/index.php
+++ fixlist/GEN/notes/nicole/index.php
@@ -1,4 +1,5 @@
 <ul>
   <li>https://www.reddit.com/r/dataisbeautiful/comments/as3si5/when_were_the_best_films_ever_released_depends/</li>
   <li>https://www.reddit.com/r/funny/comments/at6hnv/i_solved_this_maze_on_the_first_try/</li>
+  <li>https://www.reddit.com/r/OldSchoolCool/comments/auatky/today_my_grandfather_handed_me_his_1950s_journal/</li>
 </ul>
