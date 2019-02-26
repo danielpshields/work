605c75ea706637e2c17dc85d1aa30374d84738a1
alex catalano <alex.catalano2@gmail.com>
2019-02-26 07:18:30 -0500

    [notes] nicole blue links page

--- fixlist/GEN/notes/nicole/index.php
+++ fixlist/GEN/notes/nicole/index.php
@@ -1,5 +1,28 @@
-<ul>
-  <li>https://www.reddit.com/r/dataisbeautiful/comments/as3si5/when_were_the_best_films_ever_released_depends/</li>
-  <li>https://www.reddit.com/r/funny/comments/at6hnv/i_solved_this_maze_on_the_first_try/</li>
-  <li>https://www.reddit.com/r/OldSchoolCool/comments/auatky/today_my_grandfather_handed_me_his_1950s_journal/</li>
-</ul>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Blue Links</title>
+    <link rel="stylesheet" href="index.css">
+    <link rel="icon" type="image/x-icon" href="flax.png" />
+
+  </head>
+  <body>
+    <div class="container">
+
+      <header>
+        <img src="flax.png" alt="">
+        <h1>Blue Links</h1>
+        <div class="clear"></div>
+      </header>
+
+      <ul>
+        <li><a href="https://www.reddit.com/r/dataisbeautiful/comments/as3si5/when_were_the_best_films_ever_released_depends/" target="_blank"><span class="date">02/18</span> &#124; films by date</a></li>
+        <li><a href="https://www.reddit.com/r/funny/comments/at6hnv/i_solved_this_maze_on_the_first_try/" target="_blank"><span class="date">02/21</span> &#124; crossword maze</a></li>
+        <li><a href="https://www.reddit.com/r/OldSchoolCool/comments/auatky/today_my_grandfather_handed_me_his_1950s_journal/" target="_blank"><span class="date">02/24</span> &#124; 1950s movie reviews</a></li>
+      </ul>
+    </div><!-- container -->
+  </body>
+</html>
