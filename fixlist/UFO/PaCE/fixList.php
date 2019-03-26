12c67530dfcf2cdf9af35d35db33d573040110b4
ahuntercatalano <alex.catalano2@gmail.com>
2019-03-26 17:38:29 -0400

    [icons]

--- fixlist/UFO/PaCE/fixList.php
+++ fixlist/UFO/PaCE/fixList.php
@@ -3,3 +3,9 @@ alex gets a note input
 make the sortNav numbers links to their respective Pages -- eliminate the duplication
 show submitted status on the majors layout page
 only nicole can approve
+
+contacted state is NULL from ticket creation in multiple locations
+
+[records/pending.php] mass differentiation? only send to two in list to test
+  - remove all, and just add back one by one
+  - when email($pending) - the updated "removed" state does not revert the manually "removed"/popped student emails, leaving the pending.php field as empty
