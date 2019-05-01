4415af4b41cbcefb84b81676666914e11637453f
alex catalano <alex.catalano2@gmail.com>
2019-05-01 12:42:53 -0400

    [cc[meetingmiutes]]

---
+++ fixlist/GEN/cc/minutes/19/05/01/index.php
@@ -0,0 +1,47 @@
+<?php
+  $root = "../../../../";
+  $page = "minutes";
+  include("{$root}include/header.php");
+?>
+  <h2>Minutes: 05/01/19</h2>
+
+  <h3><a href="<?php echo $root; ?>people/index.php">Attendees</a></h3>
+  <ul>
+    <li>alex</li>
+    <li>dan</li>
+  </ul>
+
+  <h3>Old Business</h3>
+  <ul>
+    <li>action item No. 04: liason talked, good results, all the way up through LT</li>
+    <li>Cheat Sheet for Major Codes: group did not meet, dylan started writing out the codes</li>
+    <li>Can It Go Into Preview?: did not meet</li>
+    <li>Survey Responses: did meet?</li>
+  </ul>
+
+  <h3>Reports</h3>
+  <h4>Dan</h4>
+  <ul>
+    <li>alex is leaving, meeting with web services, getting that all together</li>
+    <li>beyond120: met with christine about global engagement</li>
+    <li>wordpress: updated to 5.1</li>
+    <li></li>
+  </ul>
+
+  <h3>Action Items</h3>
+  <table>
+    <tr>
+      <th>action</th>
+      <th><a href="<?php echo $root; ?>people/index.php">people</a></th>
+      <th>status</th>
+    </tr>
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+  </table>
+
+
+
+<?php include("{$root}include/footer.php"); ?>
