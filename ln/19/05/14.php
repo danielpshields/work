525a50367017020b3cac6dd58f1b704f4a65897c
ahuntercatalano <alex.catalano2@gmail.com>
2019-05-14 16:55:31 -0400

    [image][ln]

---
+++ ln/19/05/14.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>LN &#124; 190514_tuesday</title>
+    <link rel="icon" type="image/png" href="../../image/picon.png">
+    <link rel="stylesheet" href="../../../css/ln.css">
+  </head>
+  <body>
+    <div class="container">
+      <ul>
+        <li><a href="../../../index.php">index</a></li>
+      </ul>
+
+      <h1>190514_tuesday</h1>
+
+      <ul>
+        <li>baked banana bread</li>
+        <li>printed third knob for printer level adjust</li>
+        <li>coffee $<?php echo $spent = 2.84; ?></li>
+        <li>Staff Meeting: Discussed Digital Map / Inventory</li>
+        <li>Dan Shields: Coffee <?php echo $spent += 2.50; ?></li>
+        <li>Effren: Test Site discussion</li>
+        <li>Keith Baker: Access to "\\ls-web-share\"</li>
+        <li>meeting with CLAS IT, Keith; Brent and I discussed the current infrastructure</li>
+        <li>Efren: Test Site and infrastructural discussion</li>
+        <li>email: matt</li>
+        <li>scott converation about mobile and search bar / general layout</li>
+        <li>scott and Efren conversation</li>
+      </ul>
+
+      <footer>
+        <nav>
+          <ul>
+            <li><a href="13.html">last</a></li>
+            <p>&#124;</p>
+            <li><a href="15.php">next</a></li>
+            <div class="clear"></div>
+          </ul>
+        </nav>
+      </footer>
+    </div>
+  </body>
+</html>
