6a2fe5fb3c1c292dc97ef8c471ee80b3111a1996
ahuntercatalano <alex.catalano2@gmail.com>
2019-05-20 16:50:51 -0400

    [ln] (190520)

---
+++ fixlist/fixlist.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>FixList</h1>
+    <h2>WordPress</h2>
+    <ul>
+      <li>[social media] utilize resources for a cohesive network</li>
+      <li>[testing] create user roles at different levels to test access</li>
+      <li>[documentation] re: server crash - did we do something?</li>
+      <li>[mail server] allow people to post to sites from remote locations</li>
+    </ul>
+  </body>
+</html>
---
+++ ln/19/05/20.php
@@ -0,0 +1,41 @@
+<?php
+  $root  = "../../";
+  $spent = 0;
+  $total = $spent;
+  include("{$root}include/db.php");
+  $readDB = "SELECT credit FROM account";
+  $read   = mysqli_query($connection, $readDB);
+  $date = "190520_monday";
+  include("{$root}include/header.php");
+?>
+
+      <ul>
+        <li>starbucks: <?php echo $spent = 2.84; ?></li>
+        <li>password change | update doc server_log</li>
+        <li>Efren conversation, string play and public surplus</li>
+        <li>locked out contacted keith baker | password reset via Helpdesk in person | update server doc</li>
+        <li>wordpress</li>
+        <li>created Emily_Notes.xl</li>
+        <li>[study] wordpress</li>
+        <li>email account woes, credentials locked out, primary account "acatalano@advising.ufl.edu", contacted keith baker</li>
+        <li><a href="<?php echo $root; ?>../fixlist/fixlist.php" target="_blank">create work document</a></li>
+        <li>email account woes</li>
+      </ul>
+
+      <form action="<?php echo $root; ?>processor.php" method="post">
+        <input type="submit" name="spent" value="<?php echo "$" . $spent; ?>" tabindex="1">
+      </form>
+
+      <footer>
+        <nav>
+          <ul>
+            <li><a href="17.php">last</a></li>
+            <p>&#124;</p>
+            <li><a href="../../../index.php">next</a></li>
+            <div class="clear"></div>
+          </ul>
+        </nav>
+      </footer>
+    </div>
+  </body>
+</html>
