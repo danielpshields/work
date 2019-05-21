6ca9837dc0bc527286e6c05cdc3f4aa58efa25f2
alex catalano <alex.catalano2@gmail.com>
2019-05-21 00:21:33 -0400

    [study] php strings

---
+++ study/php/strings/index.php
@@ -0,0 +1,9 @@
+<?php
+
+  $text = "I save images from the Internet and shoot some of my own. I’m a geek for file organization. And I’ll talk your ear off about how you should be, too.";
+  echo substr($text, 2, 12);
+
+  echo "<br>";
+
+  echo strstr($text, "save");
+?>
