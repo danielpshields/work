f8a22074d1b064957c87ac85445db8fa35975dd7
alex catalano <alex.catalano2@gmail.com>
2019-05-25 16:12:01 -0400

    [ln] added 25

---
+++ ln/19/05/25.php
@@ -0,0 +1,35 @@
+<?php
+  $root  = "../../";
+  $spent = 0;
+  $total = $spent;
+  include("{$root}include/db.php");
+  $readDB = "SELECT credit FROM account";
+  $read   = mysqli_query($connection, $readDB);
+  $date = "";
+  include("{$root}include/header.php");
+?>
+
+      <ul>
+        <li>watched "scott pilgrim vs the world"</li>
+        <li>watched "moonlight"</li>
+        <li>[lnote]: enter item on same page (page-specific entry)</li>
+        <li>watched "babel"</li>
+      </ul>
+
+      <form action="<?php echo $root; ?>processor.php" method="post">
+        <input type="submit" name="spent" value="<?php echo "$" . $spent; ?>" tabindex="1">
+      </form>
+
+      <footer>
+        <nav>
+          <ul>
+            <li><a href=".php">last</a></li>
+            <p>&#124;</p>
+            <li><a href="../../../index.php">next</a></li>
+            <div class="clear"></div>
+          </ul>
+        </nav>
+      </footer>
+    </div>
+  </body>
+</html>
