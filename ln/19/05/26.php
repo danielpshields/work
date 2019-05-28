e8ee990b1878b0a4751a4816197bdb4740cef95e
alex catalano <alex.catalano2@gmail.com>
2019-05-28 00:08:48 -0400

    [ln]

---
+++ ln/19/05/26.php
@@ -0,0 +1,33 @@
+<?php
+  $root  = "../../";
+  $spent = 0;
+  $total = $spent;
+  include("{$root}include/db.php");
+  $readDB = "SELECT credit FROM account";
+  $read   = mysqli_query($connection, $readDB);
+  $date = "";
+  include("{$root}include/header.php");
+?>
+
+      <ul>
+        <li>terminal test | copying does destroy same names</li>
+        <li>copying [iMac] to [black]</li>
+      </ul>
+
+      <form action="<?php echo $root; ?>processor.php" method="post">
+        <input type="submit" name="spent" value="<?php echo "$" . $spent; ?>" tabindex="1">
+      </form>
+
+      <footer>
+        <nav>
+          <ul>
+            <li><a href=".php">last</a></li>
+            <p>&#124;</p>
+            <li><a href="../../../index.php">next</a></li>
+            <div class="clear"></div>
+          </ul>
+        </nav>
+      </footer>
+    </div>
+  </body>
+</html>
