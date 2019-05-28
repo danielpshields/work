521972c28f064cc27884840f92f95aa096f4b003
ahuntercatalano <alex.catalano2@gmail.com>
2019-05-28 12:52:06 -0400

    [ln] opened Tuesday (190528) [meeting] notes images

---
+++ ln/19/05/28.php
@@ -0,0 +1,35 @@
+<?php
+  $root  = "../../";
+  $spent = 0;
+  $total = $spent;
+  include("{$root}include/db.php");
+  $readDB = "SELECT credit FROM account";
+  $read   = mysqli_query($connection, $readDB);
+  $date = "";
+  include("{$root}include/header.php");
+?>
+
+      <ul>
+        <li>filled water bottle at work</li>
+        <li>wants and travel plans</li>
+        <li>[schedule] [lnote]</li>
+        <li>[meeting] Efren | <a href="<?php echo $root; ?>work/meeting/image/19/05/generalStructure.jpg">white board meeting about site structure</a></li>
+      </ul>
+
+      <form action="<?php echo $root; ?>processor.php" method="post">
+        <input type="submit" name="spent" value="<?php echo "$" . $spent; ?>" tabindex="1">
+      </form>
+
+      <footer>
+        <nav>
+          <ul>
+            <li><a href=".php">last</a></li>
+            <p>&#124;</p>
+            <li><a href="../../../index.php">next</a></li>
+            <div class="clear"></div>
+          </ul>
+        </nav>
+      </footer>
+    </div>
+  </body>
+</html>
