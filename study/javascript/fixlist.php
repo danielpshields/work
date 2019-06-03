059f5ae2ed052c745435013e45dab36b7761ecb0
ahuntercatalano <alex.catalano2@gmail.com>
2019-06-03 11:12:51 -0400

    [study] javascript

---
+++ study/javascript/fixlist.php
@@ -0,0 +1,13 @@
+
+  <h2>Variables</h2>
+
+  <ul>
+    <li>`const` is a signal that the identifier won’t be reassigned.</li>
+    <li>`let`, is a signal that the variable may be reassigned, such as a counter in a loop, or a value swap in an algorithm. It also signals that the variable will be used only in the block it’s defined in, which is not always the entire containing function.</li>
+  </ul>
+
+
+  <h2>Datatypes</h2>
+  <ul>
+    <li>"typeof" -- gets the data type</li>
+  </ul>
