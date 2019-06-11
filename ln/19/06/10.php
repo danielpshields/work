51d8d31f4c9eb0dbba31c159922141867f2df139
alex catalano <alex.catalano2@gmail.com>
2019-06-10 20:03:11 -0400

    [ln] to 8 p.m. check home commit

---
+++ ln/19/06/10.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>[ln]</h1>
+    <h2>1</h2>
+    <h2>2</h2>
+    <h2>3</h2>
+    <ul>
+      <li>drives(upload movies)      <a href="">image</a></li>
+      <li>wrote names on drive boxes <a href="">image</a></li>
+      <li>soldering: in parallel     <a href="">image</a></li>
+      <li>                           <a href="">image</a></li>
+    </ul>
+  </body>
+</html>
