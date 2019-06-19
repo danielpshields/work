82495597082d618137a5dfc90f56fcf7703be0da
alex catalano <alex.catalano2@gmail.com>
2019-06-19 00:12:42 -0400

    [ln] didnt fill it out?

---
+++ ln/19/06/18.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>[ln]</h1>
+    <h2>1</h2>
+    <h2>2</h2>
+    <ul>
+      <li>drives(upload movies)</li>
+      <li>designed bike ligh</li>
+      <li>printing bike light</li>
+      <li>charging drone batteries for potential biven arms</li>
+      <li>lnote: </li>
+    </ul>
+  </body>
+</html>
