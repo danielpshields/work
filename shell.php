6441d23dd32bb1e61d5708e31e680ff861af348e
ahuntercatalano <alex.catalano2@gmail.com>
2019-07-01 16:38:43 -0400

    [image] [shellWP]

---
+++ shell.php
@@ -0,0 +1,8 @@
+[gallery ids="1232,1233,1234"]
+
+<p><i>Dr Gerald Kisch (Pictured on the left, in the center with Director of Forensics Kellie Roberts, and on the right with Debate Alums)</i></p>
+
+<a href="https://test.clas.ufl.edu/cwoc-debate/files/award.jpg" rel="attachment wp-att-1215"><img class="alignnone wp-image-1215 size-medium" src="https://test.clas.ufl.edu/cwoc-debate/files/award-300x209.jpg" alt="award" width="300" height="209" /></a>
+<p><i>2014- Unaiza Batool, Evan Cartegena, Mikayla Hall, Simon Persico, Rajiv Varghese</i></p>
+
+<h3>For Outstanding Representation of the UF Speech and Debate Team</h3>
---
+++ shell_02.php
@@ -0,0 +1,117 @@
+<p>As a team, we give out several awards every year including awards given out to competitors at tournaments we host and awards given out to individuals who have helped shape the team. The links on the left will take you to the specific awards where you will find a description of the award and a list of the individuals who have received it.</p>
+
+<ul>
+  <li><a href="#">Dr. Gerald Kish Memorial Award</a></li>
+  <li><a href="#">Ryan Rimmer Memorial Award</a></li>
+  <li><a href="#">Scott Ogden Memorial Pentathalon Award</a></li>
+  <li><a href="#">The Scott Ogden Memorial Award</a></li>
+  <li><a href="#">Tom Boyer Memorial Varsity Debate Speaker Award</a></li>
+</ul>
+
+
+
+<!--
+
+The Scott Ogden Memorial Award
+2014- Rebecca Steiner
+2014- Andrew Montee
+2014- Idania Hererra Blankenship
+2013- UF Student Government Finance
+2012- Kris Willis
+2011- Colonel Robert Ely Jr.
+2010- Melissa Tyrone (UF Foundation)
+2009- Gary Winslett
+2008- Marna Weston
+2007- Amy Martinelli
+2006- Lauren Fackender
+2005- Erik Crocker
+2004- Jeff Hannan
+2003- Frank Irizarry
+2002- Dr. Gerald Kish
+2002- Josh Heller
+2001- Elenore Cotter Klinger
+2000- Dr. Jane Y. Douglas
+1999- Rob Klinger
+1998- Andy Bogeajis
+1997- Scott Newsom
+
+Ryan Rimmer Memorial Award
+2014 Brianna Mahoney
+2013 Joy Minchin
+2012 Jon Prescott
+2011 Alexandra Gonzalez
+2010 Lauren Davidson
+2009 Ben Sunshine
+2009 Alexia Ash
+2008 Marissa Silber
+2007 Colin Rawls
+2006 Raju Vyas
+2005 Amy Martinelli
+2004 Jeff Hannan
+2003 Christian Acosta
+
+<a href="https://test.clas.ufl.edu/cwoc-debate/files/Gerald-Pic.jpg" rel="attachment wp-att-1232"><img class="alignnone size-thumbnail wp-image-1232" src="https://test.clas.ufl.edu/cwoc-debate/files/Gerald-Pic-150x150.jpg" alt="Gerald Pic" width="150" height="150" /></a> <a href="https://test.clas.ufl.edu/cwoc-debate/files/kelliegerald.jpg" rel="attachment wp-att-1233"><img class="alignnone size-thumbnail wp-image-1233" src="https://test.clas.ufl.edu/cwoc-debate/files/kelliegerald-150x150.jpg" alt="kellie&amp;gerald" width="150" height="150" /></a> <a href="https://test.clas.ufl.edu/cwoc-debate/files/lucasgerald.jpg" rel="attachment wp-att-1234"><img class="alignnone size-thumbnail wp-image-1234" src="https://test.clas.ufl.edu/cwoc-debate/files/lucasgerald-150x150.jpg" alt="lucasgerald" width="150" height="150" /></a>
+
+Dr Gerald Kisch (Pictured on the left, in the center with Director of Forensics Kellie Roberts, and on the right with Debate Alums)
+
+&nbsp;
+
+2014- Unaiza Batool, Evan Cartegena, Mikayla Hall, Simon Persico, Rajiv Varghese
+
+<a href="https://test.clas.ufl.edu/cwoc-debate/files/award.jpg" rel="attachment wp-att-1215"><img class="alignnone wp-image-1215 size-medium" src="https://test.clas.ufl.edu/cwoc-debate/files/award-300x209.jpg" alt="award" width="300" height="209" /></a>
+
+
+Scott Ogden Memorial Pentathalon Award
+
+2014- Kendall Gray, Florida State University
+
+<a href="https://test.clas.ufl.edu/cwoc-debate/files/DSCN8280-e1403024914931.jpg"><img class="alignnone size-thumbnail wp-image-174" src="https://test.clas.ufl.edu/cwoc-debate/files/DSCN8280-e1403024914931-150x150.jpg" alt="DSCN8280" width="150" height="150" /></a>
+
+2013- Mikayla Hall, University of Florida
+
+<a href="https://test.clas.ufl.edu/cwoc-debate/files/1809_612668612093200_94125881_n.jpg"><img class="alignnone size-thumbnail wp-image-176" src="https://test.clas.ufl.edu/cwoc-debate/files/1809_612668612093200_94125881_n-150x150.jpg" alt="1809_612668612093200_94125881_n" width="150" height="150" /></a>
+
+2012- Thad Fitzpatrick, University of Alabama
+
+2010- Blake Williams, University of Alabama
+
+<a href="https://test.clas.ufl.edu/cwoc-debate/files/blake-williams.jpg" rel="attachment wp-att-1241"><img class="alignnone size-thumbnail wp-image-1241" src="https://test.clas.ufl.edu/cwoc-debate/files/blake-williams-150x150.jpg" alt="blake williams" width="150" height="150" /></a>
+
+2009- Angeles Burke, University of Alabama
+
+2008- Taylor Wood, University of Mississippi
+
+2007- Jeff Galtiao, Florida  Community College at Jacksonville
+
+2006- Ashley Mack, Arizona State University
+
+2005- Shanele Raggencia, University of South Florida
+
+2004- John Ashbury, Florida Stat University
+
+2003- Adam Luther, University of West Florida
+
+2001- Kirk Moore, Morehouse College
+
+2000- Paige White, University of Alabama
+
+1999- Tharon Moore, Santa Fe Community College
+
+<a href="https://test.clas.ufl.edu/cwoc-debate/files/tharon-moore-e1409076928786.jpg" rel="attachment wp-att-1242"><img class="alignnone size-thumbnail wp-image-1242" src="https://test.clas.ufl.edu/cwoc-debate/files/tharon-moore-e1409076928786-150x150.jpg" alt="tharon moore" width="150" height="150" /></a>
+
+1998- Nicole Fuller, University of Alabama
+
+
+Tom Boyer Memorial Varsity Debate Speaker Award
+
+2012- Jon Zaikowski, Wake Forest University
+
+2007- Adam Schmidt, University of Georgia
+
+2006- Kurt Zemlicka, Wake Forest University
+
+1999- Debbie Prieto, University of Miami
+
+<a href="https://test.clas.ufl.edu/cwoc-debate/files/debbie-prieto.jpg" rel="attachment wp-att-1239"><img class="alignnone size-thumbnail wp-image-1239" src="https://test.clas.ufl.edu/cwoc-debate/files/debbie-prieto-150x150.jpg" alt="debbie prieto" width="150" height="150" /></a>
+
+1998- Caitrin S. Muldoon, Florida State University -->
