411cffb1cf766c0e884306ded02ea5622f56f402
ahuntercatalano <alex.catalano2@gmail.com>
2019-07-03 11:10:52 -0400

    [image]

--- shell.php
+++ shell.php
@@ -1,8 +1,27 @@
-[gallery ids="1232,1233,1234"]
+<h3>2012</h3>
+<ul>
+ 	<li>Jon Zaikowski
+<i>Wake Forest University</i></li>
+</ul>
+<h3>2007</h3>
+<ul>
+ 	<li>Adam Schmidt
+<i>University of Georgia</i></li>
+</ul>
+<h3>2006</h3>
+<ul>
+ 	<li>Kurt Zemlicka
+<i>Wake Forest University</i></li>
+</ul>
+<h3>1999</h3>
+<img src="https://test.clas.ufl.edu/cwoc-debate/files/debbie-prieto-150x150.jpg" alt="" width="150" height="150" />
 
-<p><i>Dr Gerald Kisch (Pictured on the left, in the center with Director of Forensics Kellie Roberts, and on the right with Debate Alums)</i></p>
-
-<a href="https://test.clas.ufl.edu/cwoc-debate/files/award.jpg" rel="attachment wp-att-1215"><img class="alignnone wp-image-1215 size-medium" src="https://test.clas.ufl.edu/cwoc-debate/files/award-300x209.jpg" alt="award" width="300" height="209" /></a>
-<p><i>2014- Unaiza Batool, Evan Cartegena, Mikayla Hall, Simon Persico, Rajiv Varghese</i></p>
-
-<h3>For Outstanding Representation of the UF Speech and Debate Team</h3>
+<ul>
+ 	<li>Debbie Prieto
+<i>University of Miami</i></li>
+</ul>
+<h3>1998</h3>
+<ul>
+ 	<li>Caitrin S. Muldoon
+<i>Florida State University</i></li>
+</ul>
