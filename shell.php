1ba564b4dae92b3b4e8dd80eceb9855525097478
ahuntercatalano <alex.catalano2@gmail.com>
2019-07-03 16:57:34 -0400

    [image] shell

--- shell.php
+++ shell.php
@@ -1,27 +1,15 @@
-<h3>2012</h3>
-<ul>
- 	<li>Jon Zaikowski
-<i>Wake Forest University</i></li>
-</ul>
-<h3>2007</h3>
-<ul>
- 	<li>Adam Schmidt
-<i>University of Georgia</i></li>
-</ul>
-<h3>2006</h3>
-<ul>
- 	<li>Kurt Zemlicka
-<i>Wake Forest University</i></li>
-</ul>
-<h3>1999</h3>
-<img src="https://test.clas.ufl.edu/cwoc-debate/files/debbie-prieto-150x150.jpg" alt="" width="150" height="150" />
 
-<ul>
- 	<li>Debbie Prieto
-<i>University of Miami</i></li>
-</ul>
-<h3>1998</h3>
-<ul>
- 	<li>Caitrin S. Muldoon
-<i>Florida State University</i></li>
-</ul>
+<a href="https://test.clas.ufl.edu/cwoc-debate/files/71670_271649803000774_979145999_n.jpg"><img src="https://test.clas.ufl.edu/cwoc-debate/files/71670_271649803000774_979145999_n.jpg?w=168" alt="71670_271649803000774_979145999_n" width="168" height="300" /></a>
+
+<a href="https://test.clas.ufl.edu/cwoc-debate/files/1009987_271649783000776_588699329_n.jpg"><img src="https://test.clas.ufl.edu/cwoc-debate/files/1009987_271649783000776_588699329_n.jpg?w=225" alt="1009987_271649783000776_588699329_n" width="225" height="300" /></a>
+
+<a href="https://test.clas.ufl.edu/cwoc-debate/files/1010086_271649913000763_1200454664_n.jpg"><img src="https://test.clas.ufl.edu/cwoc-debate/files/1010086_271649913000763_1200454664_n.jpg?w=225" alt="1010086_271649913000763_1200454664_n" width="225" height="300" /></a>
+
+<a href="https://test.clas.ufl.edu/cwoc-debate/files/1798844_271649869667434_601353525_n.jpg"><img src="https://test.clas.ufl.edu/cwoc-debate/files/1798844_271649869667434_601353525_n.jpg?w=225" alt="1798844_271649869667434_601353525_n" width="225" height="300" /></a>
+
+<a href="https://test.clas.ufl.edu/cwoc-debate/files/1800412_271649889667432_777708641_n.jpg"><img src="https://test.clas.ufl.edu/cwoc-debate/files/1800412_271649889667432_777708641_n.jpg?w=225" alt="1800412_271649889667432_777708641_n" width="225" height="300" /></a>
+
+<a href="https://test.clas.ufl.edu/cwoc-debate/files/1901145_271649839667437_313077479_n.jpg"><img src="https://test.clas.ufl.edu/cwoc-debate/files/1901145_271649839667437_313077479_n.jpg?w=225" alt="1901145_271649839667437_313077479_n" width="225" height="300" /></a>
+
+
+<i>About six of us met up on Sunday evening and went to the Humane Society of Alachua County. We walked dogs for awhile and when we were done they allowed us to play with the dogs in play areas. Evan did a great job refilling the water bowls of all the thirsty dogs. Finally, we paid the cats a visit and gave them some much needed love and attention. Go gators!</i>
